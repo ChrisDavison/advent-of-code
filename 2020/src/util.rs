d645761aaaffd402ac727581bed633ed592c512b
Chris Davison <c.jr.davison@gmail.com>
2020-12-03 15:04:02 +0000

    Simplify letter counter

--- 2020/src/util.rs
+++ 2020/src/util.rs
@@ -2,13 +2,9 @@ use std::collections::HashMap;
 
 pub fn count_letters<'a>(lines: &str) -> HashMap<char, usize> {
     let mut counter = HashMap::new();
-    // for line in lines
-    for element in lines.chars() {
-        if let Some(x) = counter.get_mut(&element) {
-            *x += 1;
-        } else {
-            counter.insert(element, 1);
-        }
+    for letter in lines.chars() {
+        let count = counter.entry(letter).or_insert(0);
+        *count += 1;
     }
     counter
 }
