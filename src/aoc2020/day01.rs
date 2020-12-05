bbe02394ae0614071e3395c78ac0c1fd87bf1a0e
Chris Davison <c.jr.davison@gmail.com>
2020-12-05 19:01:38 +0000

    Tidying up day1

--- src/aoc2020/day01.rs
+++ src/aoc2020/day01.rs
@@ -2,40 +2,43 @@ use crate::part::Part;
 use anyhow::Result;
 
 pub fn day1(data: &[&str], part: Part) -> Result<()> {
-    // Part 1
-    // Find 2 entries that sum to 2020, and multiply them
-    //
-    // Part 2
-    // Find 3 entries that sum to 2020, and multiply them
     let lines: Vec<i64> = data
         .iter()
         .map(|x| x.parse())
         .filter_map(|x| x.ok())
         .collect();
+    let result = match part {
+        Part::One => part1(&lines),
+        Part::Two => part2(&lines),
+    };
+    println!("1.{} - {}", part, result);
+    Ok(())
+}
+
+pub fn part1(lines: &[i64]) -> i64 {
     let mut result = 0;
-    'outer: for i in 0..lines.len() {
-        let el_i = lines[i];
-        for j in i..lines.len() {
-            let el_j = lines[j];
-            match part {
-                Part::One => {
-                    if el_i + el_j == 2020 {
-                        result = el_i * el_j;
-                        break 'outer;
-                    }
-                }
-                Part::Two => {
-                    for k in j..lines.len() {
-                        let el_k = lines[k];
-                        if el_i + el_j + el_k == 2020 {
-                            result = el_i * el_j * el_k;
-                            break 'outer;
-                        }
-                    }
+    'outer: for (i, el_i) in lines.iter().enumerate() {
+        for el_j in &lines[i..] {
+            if el_i + el_j == 2020 {
+                result = el_i * el_j;
+                break 'outer;
+            }
+        }
+    }
+    result
+}
+
+pub fn part2(lines: &[i64]) -> i64 {
+    let mut result = 0;
+    'outer: for (i, el_i) in lines.iter().enumerate() {
+        for el_j in &lines[i..] {
+            for el_k in &lines[i + 1..] {
+                if el_i + el_j + el_k == 2020 {
+                    result = el_i * el_j * el_k;
+                    break 'outer;
                 }
             }
         }
     }
-    println!("1.{} - {}", part, result);
-    Ok(())
+    result
 }
