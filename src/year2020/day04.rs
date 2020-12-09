6148f7c2ac5f3abc95d47ba822396d7aaa155254
Chris Davison <c.jr.davison@gmail.com>
2020-12-09 22:35:52 +0000

    d4: heavy refactor. validate during parse

--- src/year2020/day04.rs
+++ src/year2020/day04.rs
@@ -1,325 +1,204 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::collections::{HashMap, HashSet};
-use thiserror::Error;
+use std::str::FromStr;
 
 const DAY: usize = 4;
-
-#[allow(dead_code)]
-#[derive(Error, Debug)]
-enum ParsePassportError {
-    #[error("Passport had an empty field")]
-    NoField,
-    #[error("Passport had an empty value")]
-    NoValue,
-}
 
 enum Height {
     Cm(usize),
     Inch(usize),
 }
 
-struct Passport<'a> {
-    ecl: &'a str,
-    hcl: &'a str,
+impl std::fmt::Display for Height {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Height::Cm(n) => write!(f, "{} Cm", n),
+            Height::Inch(n) => write!(f, "{} Inches", n),
+        }
+    }
+}
+
+impl std::fmt::Debug for Height {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Height::Cm(n) => write!(f, "{} Cm", n),
+            Height::Inch(n) => write!(f, "{} Inches", n),
+        }
+    }
+}
+
+impl FromStr for Height {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match Some(s.split_at(s.len() - 2))
+            .and_then(|(val, unit)| Some((val.parse::<usize>().ok()?, unit)))
+        {
+            Some((x @ 150..=193, "cm")) => Ok(Height::Cm(x)),
+            Some((x @ 59..=76, "in")) => Ok(Height::Inch(x)),
+            _ => Err(anyhow!("Bad height")),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Passport {
+    ecl: String,
+    hcl: String,
     eyr: usize,
     byr: usize,
     iyr: usize,
     hgt: Height,
-    pid: &'a str,
-    cid: Option<&'a str>,
+    pid: String,
+    cid: Option<String>,
 }
 
-impl<'a> std::str::FromStr for Passport<'a> {
-    type Err = ParsePassportError;
+impl FromStr for Passport {
+    type Err = anyhow::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn from_str(s: &str) -> Result<Self> {
+        // let mut fields: HashMap<&str, &str> = HashMap::new();
+        let (mut ecl, mut hcl, mut pid) = (String::new(), String::new(), String::new());
+        let mut cid = None;
+        let mut hgt: Height = Height::Cm(0);
+        let (mut byr, mut iyr, mut eyr): (usize, usize, usize) = (0, 0, 0);
+        for entry in s.split_whitespace() {
+            let mut parts = entry.split(':');
+            let field = parts.next().ok_or(anyhow!("No field"))?;
+            let value = parts.next().ok_or(anyhow!("No value"))?;
+            match field {
+                "ecl" => ecl = passport_validator::validate_ecl(value)?,
+                "hcl" => hcl = passport_validator::validate_hcl(value)?,
+                "eyr" => eyr = passport_validator::validate_eyr(value)?,
+                "byr" => byr = passport_validator::validate_byr(value)?,
+                "iyr" => iyr = passport_validator::validate_iyr(value)?,
+                "hgt" => hgt = value.parse::<Height>()?,
+                "pid" => pid = passport_validator::validate_pid(value)?,
+                "cid" => cid = Some(value.to_string()),
+                _ => continue,
+            }
+        }
         Ok(Passport {
-            ecl: "",
-            hcl: "",
-            eyr: 0,
-            byr: 0,
-            iyr: 0,
-            hgt: Height::Cm(150),
-            pid: "",
-            cid: None,
+            ecl: ecl,
+            hcl: hcl,
+            eyr: eyr,
+            byr: byr,
+            iyr: iyr,
+            hgt: hgt,
+            pid: pid,
+            cid: cid,
         })
     }
 }
 
-impl<'a> Passport<'a> {
-    fn new(s: &'a str) -> &Passport {
-        &Passport {
-            ecl: "",
-            hcl: "",
-            eyr: 0,
-            byr: 0,
-            iyr: 0,
-            hgt: Height::Cm(150),
-            pid: "",
-            cid: None,
-        }
-    }
-}
-
-pub fn solve() -> Result<()> {
+pub fn solve() -> anyhow::Result<()> {
     let data = std::fs::read_to_string(format!("input/day{}.txt", DAY))?;
-    let passports: Vec<_> = data
+    let n_simple_passports = data
         .split("\n\n")
-        .map(parse_passport)
+        .map(|entry| has_all_fields(entry))
+        .count();
+    let passports: Vec<Passport> = data
+        .split("\n\n")
+        .map(|entry| entry.parse())
         .filter_map(|x| Some(x.ok()?))
         .collect();
-    let n_valid_passports = passports
-        .iter()
-        .filter(|passport| passport_validator::part1(passport))
-        .count();
-    let n_valid_passports2 = passports
-        .iter()
-        .filter(|passport| passport_validator::part2(passport))
-        .count();
-    println!("2020 {}-1 -> {}", DAY, n_valid_passports);
-    println!("2020 {}-2 -> {}", DAY, n_valid_passports2);
+    println!("2020 {}-1 -> {}", DAY, n_simple_passports);
+    println!("2020 {}-2 -> {}", DAY, passports.len());
     Ok(())
 }
 
-fn parse_passport(s: &str) -> Result<HashMap<&str, &str>> {
+fn has_all_fields(entry: &str) -> bool {
     let mut fields: HashMap<&str, &str> = HashMap::new();
-    for entry in s.split_whitespace() {
+    for entry in entry.split_whitespace() {
         let mut parts = entry.split(':');
-        let field = parts.next().ok_or(ParsePassportError::NoField)?;
-        let value = parts.next().ok_or(ParsePassportError::NoValue)?;
+        let field = match parts.next() {
+            Some(f) => f,
+            None => return false,
+        }; //.ok_or(ParsePassportError::NoField)?;
+        let value = match parts.next() {
+            Some(v) => v,
+            None => return false,
+        }; //.ok_or(ParsePassportError::NoValue)?;
         if !field.is_empty() {
             fields.insert(field, value);
         }
     }
-    Ok(fields)
+    let necessary: HashSet<_> = ["hgt", "pid", "eyr", "iyr", "ecl", "hcl", "byr"]
+        .iter()
+        .cloned()
+        .collect();
+    necessary.is_subset(&fields.keys().copied().collect())
 }
 
 mod passport_validator {
     use super::*;
-    // cid (Country ID) - ignored, missing or not.
-    type FieldValidator = dyn Fn(&str) -> bool;
-    const VALIDATORS: &[(&str, &FieldValidator)] = &[
-        ("hgt", &is_valid_height),
-        ("pid", &is_valid_pid),
-        ("eyr", &is_valid_eyr),
-        ("byr", &is_valid_byr),
-        ("iyr", &is_valid_iyr),
-        ("ecl", &is_valid_ecl),
-        ("hcl", &is_valid_hcl),
-    ];
 
-    pub fn part1(fields: &HashMap<&str, &str>) -> bool {
-        let necessary: HashSet<_> = ["hgt", "pid", "eyr", "iyr", "ecl", "hcl", "byr"]
-            .iter()
-            .cloned()
-            .collect();
-        necessary.is_subset(&fields.keys().map(|&x| x).collect())
-    }
-
-    pub fn part2(fields: &HashMap<&str, &str>) -> bool {
-        VALIDATORS
-            .iter()
-            .all(|(key, validate_func)| match fields.get(*key) {
-                Some(value) => validate_func(value),
-                _ => false,
-            })
-    }
-
-    pub fn is_valid_height(f: &str) -> bool {
+    pub fn validate_height(f: &str) -> bool {
+        // NO LONGER USED
+        // can just use 'parse' to get an actual Height object
         // hgt (Height) - a number followed by either cm or in:
         //     If cm, the number must be at least 150 and at most 193.
         //     If in, the number must be at least 59 and at most 76.
-        match Some(f.split_at(f.len() - 2))
-            .and_then(|(val, unit)| Some((val.parse::<i64>().ok()?, unit)))
-        {
-            Some((150..=193, "cm")) => true,
-            Some((59..=76, "in")) => true,
+        match f.parse::<Height>() {
+            Ok(_) => true,
             _ => false,
         }
     }
 
     // pid (Passport ID) - 9 digit number, with leading zeros
-    pub fn is_valid_pid(f: &str) -> bool {
-        f.chars().filter(|c| c.is_digit(10)).count() == 9
+    pub fn validate_pid(f: &str) -> Result<String> {
+        if f.chars().filter(|c| c.is_digit(10)).count() == 9 {
+            Ok(f.to_string())
+        } else {
+            Err(anyhow!("Bad passport ID"))
+        }
     }
 
     // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    pub fn is_valid_eyr(f: &str) -> bool {
-        matches!(f.parse::<i64>().ok(), Some(2020..=2030))
+    pub fn validate_eyr(f: &str) -> Result<usize> {
+        match f.parse::<usize>() {
+            Ok(x @ 2020..=2030) => Ok(x),
+            _ => Err(anyhow!("Bad expiration year")),
+        }
+        //if matches!(f.parse::<i64>().ok(), Some(2020..=2030))
     }
 
     // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    pub fn is_valid_iyr(f: &str) -> bool {
-        matches!(f.parse::<i64>().ok(), Some(2010..=2020))
+    pub fn validate_iyr(f: &str) -> Result<usize> {
+        match f.parse::<usize>() {
+            Ok(x @ 2010..=2020) => Ok(x),
+            _ => Err(anyhow!("Bad issue year")),
+        }
     }
 
     // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    pub fn is_valid_byr(f: &str) -> bool {
-        matches!(f.parse::<i64>().ok(), Some(1920..=2002))
+    pub fn validate_byr(f: &str) -> Result<usize> {
+        match f.parse::<usize>() {
+            Ok(x @ 1920..=2002) => Ok(x),
+            _ => Err(anyhow!("Bad birth year")),
+        }
     }
 
     // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    pub fn is_valid_ecl(f: &str) -> bool {
-        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&f)
+    pub fn validate_ecl(f: &str) -> Result<String> {
+        if ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&f) {
+            Ok(f.to_string())
+        } else {
+            Err(anyhow!("Bad eye colour"))
+        }
     }
 
     // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    pub fn is_valid_hcl(f: &str) -> bool {
+    pub fn validate_hcl(f: &str) -> Result<String> {
         match f.strip_prefix('#') {
-            Some(hex) => hex.chars().filter(|c| c.is_digit(16)).count() == 6,
-            _ => false,
+            Some(hex) => {
+                if hex.chars().filter(|c| c.is_digit(16)).count() == 6 {
+                    Ok(f.to_string())
+                } else {
+                    Err(anyhow!("Bad hair colour"))
+                }
+            }
+            _ => Err(anyhow!("Bad hair colour")),
         }
-    }
-}
-
-macro_rules! map(
-    { $($key:expr => $value:expr),+ } => {
-        {
-            let mut m = ::std::collections::HashMap::new();
-            $(
-                m.insert($key, $value);
-            )+
-            m
-        }
-     };
-);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn examples_part1() {
-        let tests = vec![(
-            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm",
-            true,
-        )];
-        for (test, expected) in tests {
-            assert_eq!(
-                passport_validator::part1(&parse_passport(test).unwrap()),
-                expected
-            );
-        }
-    }
-
-    #[test]
-    fn hcl() {
-        let tests = vec![
-            ("#abcdef", true),
-            ("#abcdeg", false),
-            ("#abc123", true),
-            ("#abcdeff", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_hcl(input), expected);
-        }
-    }
-
-    #[test]
-    fn ecl() {
-        let tests = vec![
-            ("amb", true),
-            ("blu", true),
-            ("brn", true),
-            ("gry", true),
-            ("grn", true),
-            ("hzl", true),
-            ("oth", true),
-            ("oth", true),
-            ("askdjaksjd", false),
-            ("123", false),
-        ];
-        for (input, expected) in tests {
-            println!("{} {}", input, expected);
-            assert_eq!(passport_validator::is_valid_ecl(input), expected);
-        }
-    }
-
-    #[test]
-    fn byr() {
-        // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-        let tests = vec![
-            ("1919", false),
-            ("abc", false),
-            ("1920", true),
-            ("2002", true),
-            ("2003", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_byr(input), expected);
-        }
-    }
-
-    #[test]
-    fn eyr() {
-        // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-        let tests = vec![
-            ("2019", false),
-            ("abc", false),
-            ("2020", true),
-            ("2030", true),
-            ("2031", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_eyr(input), expected);
-        }
-    }
-
-    #[test]
-    fn iyr() {
-        // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-        let tests = vec![
-            ("2009", false),
-            ("abc", false),
-            ("2010", true),
-            ("2020", true),
-            ("2021", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_iyr(input), expected);
-        }
-    }
-
-    #[test]
-    fn pid() {
-        let tests = vec![
-            ("123456789", true),
-            ("000456789", true),
-            ("a23456789", false),
-            ("123458", false),
-            ("", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_pid(input), expected);
-        }
-    }
-
-    #[test]
-    fn hgt() {
-        let tests = vec![
-            // Test cm
-            ("149cm", false),
-            ("150cm", true),
-            ("193cm", true),
-            ("194cm", false),
-            ("194cma", false),
-            // Test in
-            ("58in", false),
-            ("59in", true),
-            ("76in", true),
-            ("77in", false),
-            ("77ina", false),
-            // Test other
-            ("190", false),
-            ("abc", false),
-        ];
-        for (input, expected) in tests {
-            assert_eq!(passport_validator::is_valid_height(input), expected);
-        }
-    }
-
-    #[test]
-    fn random() {
-        assert_eq!('\n'.is_whitespace(), true);
     }
 }
