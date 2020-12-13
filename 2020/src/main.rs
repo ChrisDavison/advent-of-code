97f43c9f643a11dc5f9d2b17a9fd6787b1484347
Chris Davison <c.jr.davison@gmail.com>
2020-12-13 17:23:56 +0000

    day13 done

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -10,6 +10,7 @@ mod day09;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 use std::time::Instant;
 use structopt::StructOpt;
@@ -45,6 +46,7 @@ fn main() {
         (10, day10::day10),
         (11, day11::day11),
         (12, day12::day12),
+        (13, day13::day13),
     ];
 
     let start = Instant::now();
