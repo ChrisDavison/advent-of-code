4c37e66f577ed9cc4eb0b3ec0cfc1bc5379b7817
Chris Davison <c.jr.davison@gmail.com>
2020-12-15 14:07:05 +0000

    Update template; add d15 to mod and main

--- src/aoc2020/day15.rs
+++ src/aoc2020/day15.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
-pub fn dayXX() -> Result<()> {
-    let data = std::fs::read_to_string("input/XX.in")?;
+pub fn day15() -> Result<()> {
+    let data = std::fs::read_to_string("input/15.in")?;
     part_1(&data)?;
     part_2(&data)?;
     Ok(())
@@ -10,13 +10,13 @@ pub fn dayXX() -> Result<()> {
 fn part_1(data: &str) -> Result<()> {
     unimplemented!();
     let result = 0;
-    println!("AoC2020 XX.1 -> {}", result);
+    println!("AoC2020 15.1 -> {}", result);
     Ok(())
 }
 
 fn part_2(data: &str) -> Result<()> {
     unimplemented!();
     let result = 0;
-    println!("AoC2020 XX.2 -> {}", result);
+    println!("AoC2020 15.2 -> {}", result);
     Ok(())
 }
