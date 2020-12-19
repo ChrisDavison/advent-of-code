ed41f50abf3325a1e17e710dc37dcd356406a42c
Chris Davison <c.jr.davison@gmail.com>
2020-12-19 22:34:28 +0000

    Significantly tidy d19p1

--- src/aoc2020/day19.rs
+++ src/aoc2020/day19.rs
@@ -1,64 +1,121 @@
 #![allow(dead_code, unused_variables, unused_mut, unreachable_code)]
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn day19() -> Result<()> {
-    let filename = "input/19.in";
-    //let filename = "input/19.sample";
-    let data = std::fs::read_to_string(filename)?;
-    let (mut rules, mut endpoints, messages) = parse_data(&data);
-    while endpoints.len() < rules.keys().count() {
-        for (rulenum, rule) in rules.clone() {
-            if endpoints.iter().find(|s| **s == rulenum).is_some() {
+    let data = std::fs::read_to_string("input/19.in")?;
+    println!("AoC2020 19.1 -> {}", part1(&data)?);
+
+    //let data = std::fs::read_to_string("input/19-2.in")?;
+    // let data = std::fs::read_to_string("input/19-2.sample")?;
+    // println!("AoC2020 19.2 -> {}", part2(&data)?);
+    Ok(())
+}
+
+fn part1(data: &str) -> Result<String> {
+    let (mut rules, messages) = parse_data(&data);
+
+    loop {
+        let to_process = rules
+            .iter()
+            .filter(|(k, v)| !terminates(v))
+            .map(|(k, v)| k.to_string())
+            .collect::<HashSet<String>>();
+        if to_process.is_empty() {
+            break;
+        }
+        for rulenum in to_process {
+            let rule = rules[&rulenum].to_string();
+            let mut new: Vec<String> = Vec::new();
+            for part in rule.split(" ") {
+                if let Ok(n) = part.parse::<i64>() {
+                    let s = format!("( {} )", rules[part].to_string());
+                    new.push(s);
+                } else {
+                    new.push(part.to_string());
+                }
+            }
+            rules.insert(rulenum.to_string(), new.join(" "));
+        }
+    }
+
+    let re = Regex::new(&format!("\\b{}\\b", rules["0"].replace(" ", ""))).unwrap();
+
+    Ok(format!(
+        "{}",
+        messages.iter().filter(|m| re.is_match(m)).count()
+    ))
+}
+
+fn part2(data: &str) -> Result<String> {
+    let (mut rules, messages) = parse_data(&data);
+    let mut endpoints = rules
+        .iter()
+        .filter(|(k, v)| terminates(v))
+        .map(|(k, v)| k.to_string())
+        .collect::<Vec<String>>();
+
+    loop {
+        //while endpoints.len() < rules.keys().count() {
+        let mut to_process = rules
+            .iter()
+            .filter(|(k, v)| !terminates(v))
+            .map(|(k, _)| k.to_string())
+            .collect::<HashSet<String>>();
+        println!("{:?}", to_process);
+        if to_process.is_empty() {
+            break;
+        }
+
+        for rulenum in &to_process {
+            let rule = rules[rulenum].to_string();
+            if endpoints.contains(&rulenum) {
                 continue;
             }
+            let rulenum_spaced = format!(" {} ", rulenum);
+
+            println!(
+                "RULE {} === {:?}",
+                rulenum,
+                rules[rulenum].split(" ").collect::<HashSet<&str>>()
+            );
             let new = rule
                 .split(" ")
                 .map(|c| {
-                    if endpoints.contains(&c.clone().to_string()) {
-                        format!("({})", rules[&c.clone().to_string()].to_string())
+                    if rule.contains(&rulenum_spaced) || rule.ends_with(&format!(" {}", rulenum)) {
+                        let mut newrec = rule.clone();
+                        for _ in 0..5 {
+                            newrec = newrec.replace(rulenum, &format!(" ( {} ) ", &rule));
+                        }
+                        newrec = newrec.replace(&rulenum_spaced, " ");
+                        newrec = newrec.replace(rulenum, " ");
+
+                        newrec.replace("( a )", "a").replace("( b )", "b")
+                    } else if endpoints.contains(&c.clone().to_string()) {
+                        format!("( {} ) ", rules[&c.clone().to_string()].to_string())
                     } else {
                         c.clone().to_string()
                     }
                 })
                 .collect::<Vec<String>>()
                 .join(" ");
-            //println!("NO | #{} > {}  --->  {}", rulenum, rule, new);
             rules.insert(rulenum.to_string(), new.clone());
-            if new.chars().all(|c| !c.is_digit(10)) {
-                endpoints.push(rulenum);
-            }
+
+            let mut endpoints = rules
+                .iter()
+                .filter(|(k, v)| terminates(v))
+                .map(|(k, v)| k.to_string())
+                .collect::<Vec<String>>();
         }
     }
 
-    let tidied = rules["0"].replace(" ", "");
-    let tidied = tidied.replace("(a)", "a");
-    let tidied = tidied.replace("(b)", "b");
-    let re = Regex::new(&format!("\\b{}\\b", tidied)).unwrap();
-    let mut count = 0;
-    for msg in messages {
-        if re.is_match(msg) {
-            println!("{}", msg);
-            count += 1;
-        }
-    }
+    let re = Regex::new(&format!("\\b{}\\b", rules["0"].replace(" ", ""))).unwrap();
 
-    println!("{}", count);
-
-    println!("AoC2020 19.1 -> {}", part1(&data)?);
-    println!("AoC2020 19.2 -> {}", part1(&data)?);
-    Ok(())
-}
-
-fn part1(data: &str) -> Result<String> {
-    return Err(anyhow!("Part 1 not implemented"));
-    Ok(format!("{}", 0))
-}
-
-fn part2(data: &str) -> Result<String> {
-    return Err(anyhow!("Part 2 not implemented"));
-    Ok(format!("{}", 0))
+    Ok(format!(
+        "{}",
+        messages.iter().filter(|m| re.is_match(m)).count()
+    ))
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -67,29 +124,11 @@ enum Rule {
     Letter(char),
 }
 
-fn expand_rules(rules: HashMap<String, Vec<Rule>>) {
-    for k in rules.keys() {
-        //let mut new = Vec::new();
-        for subrule in &rules[k] {
-            // if matches!(rulenum, Rule::Letter(_)) {
-            //     continue;
-            // }
-            // if let Rule::Letter(_) = rulenum {
-            //     continue;
-            // }
-            println!("{} {:?}", k, subrule);
-        }
-        println!();
-        // rules[k] = new;
-    }
-}
-
-fn parse_data<'a>(data: &'a str) -> (HashMap<String, String>, Vec<String>, Vec<&'a str>) {
+fn parse_data<'a>(data: &'a str) -> (HashMap<String, String>, Vec<&'a str>) {
     let mut parts = data.split("\n\n");
 
     let mut rules = HashMap::new();
     let mut messages = Vec::new();
-    //let mut endpoints = Vec::new();
     for line in parts.next().expect("No rules given").lines() {
         let mut lineparts = line.split(':');
         let num = lineparts.next().unwrap();
@@ -104,17 +143,15 @@ fn parse_data<'a>(data: &'a str) -> (HashMap<String, String>, Vec<String>, Vec<&
         );
     }
 
-    let endpoints: Vec<String> = rules
-        .iter()
-        .filter(|(num, rule)| rule.chars().all(|c| !c.is_digit(10)))
-        .map(|(k, _)| k.to_string())
-        .collect();
-
     //println!("ENDPOINTS {:?}", endpoints);
 
     for line in parts.next().expect("No messages given").lines() {
         messages.push(line);
     }
 
-    (rules, endpoints, messages)
+    (rules, messages)
+}
+
+fn terminates(pattern: &str) -> bool {
+    pattern.chars().all(|c| "(  )|ab".contains(c))
 }
