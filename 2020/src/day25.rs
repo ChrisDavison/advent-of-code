df02b8622a232aa1f536c7648bdad987351c52cf
Chris Davison <c.jr.davison@gmail.com>
2020-12-25 10:16:00 +0000

    25: Only do the modification for one number

--- 2020/src/day25.rs
+++ 2020/src/day25.rs
@@ -1,43 +1,32 @@
-#![allow(dead_code, unused_variables, unused_mut, unreachable_code)]
 use crate::prelude::*;
 
 pub fn day25() -> Result<()> {
-    //let data = SAMPLE;
-    let (card, door) = SAMPLE;
+    // let (card, door) = SAMPLE;
     let (card, door) = INPUT;
-    println!("AoC2020 25.1 -> {}", part1(card, door)?);
-    // println!("AoC2020 25.2 -> {}", part2(&INPUT)?);
-    Ok(())
-}
-
-fn part1(card: usize, door: usize) -> Result<String> {
-    // return Err(anyhow!("Part 1 not implemented"));
     let subj = 7;
     let divi = 20201227;
     let mut start = 1;
-    let mut start2 = 1;
-    let mut n_iter_card = 0;
-    let mut n_iter_door = 0;
-    while start != card || start2 != door {
-        if start != card {
-            start = start * subj % divi;
-            n_iter_card += 1;
+    let subj2: usize;
+    let mut iters2 = 0;
+    loop {
+        if start == card {
+            subj2 = door;
+            break;
         }
-        if start2 != door {
-            start2 = start2 * subj % divi;
-            n_iter_door += 1;
+        if start == door {
+            subj2 = card;
+            break;
         }
+        start = start * subj % divi;
+        iters2 += 1;
     }
     start = 1;
-    for i in 0..n_iter_card {
-        start = start * door % divi;
+    for _ in 0..iters2 {
+        start = start * subj2 % divi;
     }
-    Ok(format!("{}", start))
-}
 
-fn part2(data: &str) -> Result<String> {
-    return Err(anyhow!("Part 2 not implemented"));
-    //Ok(format!("{}", 0))
+    println!("AoC2020 25 -> {}", start);
+    Ok(())
 }
 
 #[allow(dead_code)]
