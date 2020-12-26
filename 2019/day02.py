469636e5a3c16e0ccd978f97769a8b3bd456a528
Chris Davison <c.jr.davison@gmail.com>
2020-12-26 23:58:19 +0000

    WIP day5

--- 2019/day02.py
+++ 2019/day02.py
@@ -1,41 +1,33 @@
 from prelude import *
+from machine import Machine
 
+def run_with_replace(data, a, b):
+    data[1] = a
+    data[2] = b
+    m = Machine(data)
+    ans = m.run()
+    return ans
 
-def day02():
-    data = [int(d) for d in open('input/02').read().split(',')]
-    part1(data.copy())
-    part2(data)
 
 def part1(data):
-    data[1] = 12
-    data[2] = 2
-    p1 = run(data)
-    print(p1)
-    clip.copy(p1)
+    ans = run_with_replace(data.copy(), 12, 2)
+    print(ans)
+    clip.copy(ans)
+    
 
 def part2(data, target=19690720):
     for i in range(100):
         for j in range(100):
-            data2 = data.copy()
-            data2[1] = i
-            data2[2] = j
-            out = run(data2)
+            out = run_with_replace(data.copy(), i, j)
             if out == target:
                 p2 = 100 * i + j
                 print(f"{p2}")
                 clip.copy(f"{p2}")
-                return
+                return 
 
-def run(opcodes, position=0):
-    if opcodes[position] == 99:
-        return opcodes[0]
-    op = opcodes[position]
-    a = opcodes[opcodes[position+1]]
-    b = opcodes[opcodes[position+2]]
-    c = opcodes[position+3]
-    if op == 1:
-        opcodes[c] = a + b
-    else:
-        opcodes[c] = a * b
-    return run(opcodes, position+4)
+
+if __name__ == "__main__":
+    data = [int(d) for d in open('input/02').read().split(',')]
+    part1(data)
+    part2(data)
 
