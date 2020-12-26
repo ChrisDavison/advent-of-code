8333670497e5c0e9e78ca278cac9d0625eb890ff
Chris Davison <c.jr.davison@gmail.com>
2020-12-26 16:36:09 +0000

    Bump version

--- 2020/template.rs
+++ 2020/template.rs
@@ -1,26 +1,22 @@
 #![allow(dead_code, unused_variables, unused_mut, unreachable_code)]
 use crate::prelude::*;
 
-pub fn dayXX() -> Result<()> {
-    println!("AoC2020 XX.1 -> {}", part1(&INPUT)?);
-    println!("AoC2020 XX.2 -> {}", part2(&INPUT)?);
+fn main() -> Result<()> {
+    let input = include_str!("input");
+    println!("2020 XX.1 -> {}", part1(&input)?);
+    println!("2020 XX.2 -> {}", part2(&input)?);
     Ok(())
 }
 
 fn part1(data: &str) -> Result<String> {
-    Err(anyhow!("Part 1 not implemented"))
-    //Ok(format!("{}", 0))
+    return Err(anyhow!("Part 1 not implemented"));
+    Ok(format!("{}", 0))
 }
 
 fn part2(data: &str) -> Result<String> {
-    Err(anyhow!("Part 2 not implemented"))
-    //Ok(format!("{}", 0))
+    return Err(anyhow!("Part 2 not implemented"));
+    Ok(format!("{}", 0))
 }
-
-#[allow(dead_code)]
-const INPUT: &str = r#"
-
-"#;
 
 #[allow(dead_code)]
 const SAMPLE: &str = r#"
