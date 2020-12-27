c08c39c920874960521869d014448051513c35ab
Chris Davison <c.jr.davison@gmail.com>
2020-12-27 12:40:09 +0000

    Day5 done

--- 2019/day05.py
+++ 2019/day05.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
 from prelude import *
 from collections import defaultdict, Counter
-from machine import Machine
+from intcode import IntCode
+import logging
 
-data = [int(l) for l in open('input/05').read().split(',')]
-print(len(data))
-# print(data[:5])
 
-m = Machine(data)
-print(m.run())
-# Part1
+if __name__ == "__main__":
 
-# Part2
+    data = [l for l in open('input/05').read().split(',')]
 
+    m = IntCode(data.copy(), inputs=[1], level=logging.WARNING)
+    m.run()
+    print("2019 5.1 ->", m.last_output())
+
+    m = IntCode(data.copy(), inputs=[5], level=logging.WARNING)
+    m.run()
+    print("2019 5.2 ->", m.last_output())
 
---
+++ 2019/intcode.py
@@ -0,0 +1,159 @@
+import logging
+import pyperclip as clip
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ],
+)
+class IntCode:
+    """Represents an IntCode machine"""
+    def __init__(self, opcodes, **kwargs):
+        self.ops = [int(i) for i in opcodes]
+        self.ptr = 0
+        self.halted = False
+        self.mem = dict()
+        self.modes = []
+        self.inputs = kwargs.get("inputs", [])
+        self.outputs = []
+        self.logger = logging.getLogger("intcode")
+        self.logger.setLevel(kwargs.get("level", logging.DEBUG))
+        self.logger.debug("INPUTS %s", self.inputs)
+
+    def run(self):
+        while not self.halted:
+            self.logger.debug("STEP: %s", self.ops[self.ptr:self.ptr+4])
+            self.one_step()
+        return self.ops[0]
+
+    def parse_instr(self, s):
+        s = str(s).rjust(5, "0")
+        return [int(i) for i in str(s).rjust(5, "0")]
+
+    def one_step(self):
+        if self.halted:
+            return self.ops[0]
+        intcode = self.parse_instr(self.ops[self.ptr])
+        self.ptr += 1
+        
+
+        instrs = {
+            1: self.add,
+            2: self.mul,
+            3: self.save,
+            4: self.read,
+            5: self.jump_true,
+            6: self.jump_false,
+            7: self.less_than,
+            8: self.equals,
+            99: self.halt,
+        }
+
+        opkey = intcode[-2] * 10 + intcode[-1]
+        try:
+            op = instrs[opkey](intcode)
+        except Exception as E:
+            raise Exception(f"BAD OP CODE `{opkey}` - ops {self.ops[self.ptr:self.ptr+5]}")
+        if self.ptr >= len(self.ops):
+            raise Exception("Pointer past end of operations")
+
+    def get(self, offset, mode=0):
+        """
+        Read val from memory
+
+        if mode == I (immediate), use this as value
+        else, treat it as first memory address, and return ops[val]
+        """
+        if mode == 0:
+            return int(self.ops[self.ops[self.ptr + offset]])
+        else:
+            return int(self.ops[self.ptr + offset])
+    
+    def set(self, offset, value):
+        self.ops[self.ops[self.ptr + offset]] = value
+
+    def jump_true(self, intcode):
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        if first != 0:
+            self.ptr = second
+        else:
+            self.ptr += 2
+
+    def jump_false(self, intcode):
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        if first == 0:
+            self.ptr = second
+        else:
+            self.ptr += 2
+
+    def less_than(self, intcode):
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        self.set(2, 1 if first < second else 0)
+        self.ptr += 3
+
+    def equals(self, intcode):
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        self.set(2, 1 if first == second else 0)
+        self.ptr += 3
+
+    def add(self, intcode):
+        """
+        ops[c] = first + second
+        """
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        store_at = int(self.ops[self.ptr+2])
+        self.logger.debug("ADD  %d + %d => ops[%s]", first, second, store_at)
+        self.set(2, first + second)
+        self.ptr += 3
+
+    def mul(self, intcode):
+        """
+        ops[c] = first * second
+        """
+        first = self.get(0, intcode[2])
+        second = self.get(1, intcode[1])
+        store_at = int(self.ops[self.ptr+2])
+        self.logger.debug("MULT %d * %d => ops[%d]", first, second, store_at)
+        self.set(2, first * second)
+        self.ptr += 3
+
+    def save(self, intcode):
+        """
+        ops[first] = ...input...
+        """
+        store_at = int(self.ops[self.ptr])
+        popped = self.inputs.pop()
+        self.logger.debug("SAVE %s => ops[%s]", popped, store_at)
+        self.logger.info("IN %s", popped)
+        self.set(0, popped)
+        self.ptr += 1
+
+    def read(self, intcode):
+        """
+        OUTPUT = ops[first]
+        """
+        mem_addr = self.get(0, intcode[2])
+        self.logger.debug("READ ops[%s] => %s", self.ops[self.ptr], mem_addr)
+        self.logger.info("OUT %s", mem_addr)
+        self.outputs.append(mem_addr)
+        clip.copy(mem_addr)
+        self.ptr += 1
+
+    def last_output(self):
+        return self.outputs[-1]
+
+    def halt(self, _intcode):
+        """
+        STOP PROGRAM and output ops[0]
+        """
+        self.halted = True
+        return self.ops[0]
+
