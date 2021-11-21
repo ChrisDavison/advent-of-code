c43488e1e838141f51e7ff210fe6c08cc270be8c
Chris Davison <c.jr.davison@gmail.com>
2021-11-21 11:18:02 +0000

    Template for 2021, in python

---
+++ 2021/day16.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from prelude import *
+
+def part1(args):
+    pass
+
+def part2(args):
+    pass
+
+
+data = [int(l) for l in open('input/16').read().splitlines()]
+timed("2021 16.1", part1, args)
+timed("2021 16.2", part2, args)
