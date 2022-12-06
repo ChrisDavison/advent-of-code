ab292740c0ee69aff537d01da5c425b714d2d17c
Chris Davison <c.jr.davison@gmail.com>
2022-12-06 09:39:41 +0000

    day6 done

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,17 +1,40 @@
-ex = """
-
-"""
-
-def parse(data):
-    pass
+ex = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+ex2 = """bvwbjplbgvbhsrlpgdmjqwftvncz"""
+ex3 = """nppdvjthqldpwncqszvftbrmjlhg"""
+ex4 = """nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"""
+ex5 = """zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"""
 
 def part1(data):
-    pass
+    # 4 consecutive unique characters
+    n = 4
+    for i in range(len(data)-n):
+        # print(data[i:i+4])
+        if len(set(data[i:i+n])) == n:
+            return i+n
+    return -1
 
 def part2(data):
-    pass
+    # 14 consecutive unique characters
+    n = 14
+    for i in range(len(data)-n):
+        # print(data[i:i+4])
+        if len(set(data[i:i+n])) == n:
+            return i+n
+    return -1
 
-ex = parse(ex)
-print(ex)
+
+# print(ex)
 print(part1(ex))
+# print(part1(ex2))
+# print(part1(ex3))
+# print(part1(ex4))
+# print(part1(ex5))
+print(part1(open('inputs/06').read()))
+
 print(part2(ex))
+# print(part2(ex2))
+# print(part2(ex3))
+# print(part2(ex4))
+# print(part2(ex5))
+print(part2(open('inputs/06').read()))
+
