d789c6a862b0c9db6d2a8094163944423167a1c8
Chris Davison <c.jr.davison@gmail.com>
2023-12-05 13:55:08 +0000

    commit when starting new day

--- 2023/new_day.sh
+++ 2023/new_day.sh
@@ -16,3 +16,8 @@ rm input/day$day2 2> /dev/null
 SESSION=53616c7465645f5f72b48167ee6787c93709caa950b829051c0dce2d17a41654f74667d4a5b65ce0e32980dc766772ab75b6f78f30f6dfd9a2227325cc22a31e
 URL="https://adventofcode.com/"$year"/day/"$day1"/input"
 curl --cookie "session=$SESSION" $URL > input/day$day2
+
+git add src/day$day2.rs input/$day2 Cargo.toml
+git commit -m "Start day$day2"
+
+nvim src/day$day2.rs
