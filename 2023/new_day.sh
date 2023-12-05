fea0357f5698e3ee8a4b07c9089b9b4c2d2380ea
Chris Davison <c.jr.davison@gmail.com>
2023-12-05 14:00:59 +0000

    remove token and read from file

--- 2023/new_day.sh
+++ 2023/new_day.sh
@@ -13,7 +13,11 @@ sed -i'' -e "s/DAYNUM1/$day1/g" Cargo.toml
 
 # now get data
 rm input/day$day2 2> /dev/null
-SESSION=53616c7465645f5f72b48167ee6787c93709caa950b829051c0dce2d17a41654f74667d4a5b65ce0e32980dc766772ab75b6f78f30f6dfd9a2227325cc22a31e
+SESSION=$(cat ~/.aoc_token)
+if [[ -z "$SESSION" ]]; then
+    echo "AOC token empty. Put into ~/.aoc_token"
+    exit -1
+fi
 URL="https://adventofcode.com/"$year"/day/"$day1"/input"
 curl --cookie "session=$SESSION" $URL > input/day$day2
 
