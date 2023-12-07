0988eb950c4f85e0b4fba3e96f5e8865efb37bb6
Chris Davison <c.jr.davison@gmail.com>
2023-12-07 08:49:51 +0000

    day7 part2

--- 2023/day07.py
+++ 2023/day07.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from pandas.core.indexes.base import pprint_thing
 from utility import *
+from time import process_time_ns, time_ns
 
 SAMPLE = """32T3K 765
 T55J5 684
@@ -18,8 +19,7 @@ def cardhand1(s):
     cards, bid = s.split(" ")
     bid = int(bid)
     cardset = sorted(Counter(cards).values())[::-1]
-    rank = "123456789TJQKA"
-    cardranks = mapt(lambda x: rank.index(x), cards)
+    cardranks = mapt(lambda x: "123456789TJQKA".index(x), cards)
     match cardset:
         case [5]:
             return Hand(6, cardranks, cards, bid)
@@ -42,24 +42,27 @@ def cardhand2(s):
     bid = int(bid)
     nj, cards_noj = sum(1 for c in cards if c == 'J'), ''.join(c for c in cards if c != 'J')
     cardset = sorted(Counter(cards_noj).values())[::-1]
-    rank2 = "J123456789TQKA"
-    cardranks2 = mapt(lambda x: rank2.index(x), cards)
-    print(f"{cards}\t{cardset = :} {nj = :}")
-    match (cardset, nj):
-        case ([5], x):
-            return Hand(6, cardranks2, cards, bid)
-        case ([4, 1], x):
-            return Hand(5, cardranks2, cards, bid)
-        case ([3, 2], x):
-            return Hand(4, cardranks2, cards, bid)
-        case ([3, 1, 1], x):
-            return Hand(3, cardranks2, cards, bid)
-        case ([2, 2, 1], x):
-            return Hand(2, cardranks2, cards, bid)
-        case ([2, 1, 1, 1], x):
-            return Hand(1, cardranks2, cards, bid)
+    cardranks = mapt(lambda x: "J123456789TQKA".index(x), cards)
+    # print(f"{cards}\t{cardset = :} {nj = :}")
+    if cardset:
+        cardset[0] += nj
+    else:
+        cardset = [nj]
+    match cardset:
+        case [5]:
+            return Hand(6, cardranks, cards, bid)
+        case [4, 1]:
+            return Hand(5, cardranks, cards, bid)
+        case [3, 2]:
+            return Hand(4, cardranks, cards, bid)
+        case [3, 1, 1]:
+            return Hand(3, cardranks, cards, bid)
+        case [2, 2, 1]:
+            return Hand(2, cardranks, cards, bid)
+        case [2, 1, 1, 1]:
+            return Hand(1, cardranks, cards, bid)
         case _:
-            return Hand(0, cardranks2, cards, bid)
+            return Hand(0, cardranks, cards, bid)
 
 def p(data, cardgen):
     cards = mapl(cardgen, data.splitlines())
@@ -74,10 +77,13 @@ def f(data):
         s += score
     return s
 
-
+start = time_ns()
 print(f"1. {f(SAMPLE) = :}")
 print(f"1. {f(DATA) = :}")
+mid = time_ns()
+print(f"\t{(mid - start) / 1e6:.0f}ms")
 
+print("-----------")
 
 # --- part 2
 def f2(data):
@@ -88,6 +94,8 @@ def f2(data):
         s += score
     return s
 
-
 print(f"2. {f2(SAMPLE) = :}")
-# print(f"2. {f2(DATA) = :}")
+print(f"2. {f2(DATA) = :}")
+end = time_ns()
+print(f"\t{(end - mid) / 1e6:.0f}ms")
+print(f"Total {(end - start) / 1e6:.0f} ms")
