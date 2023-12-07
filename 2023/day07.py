73b705f5c9aad141b84efdd550e7c3a53f305ca7
Chris Davison <c.jr.davison@gmail.com>
2023-12-07 11:54:29 +0000

    use util parse function and simplify some code

--- 2023/day07.py
+++ 2023/day07.py
@@ -1,9 +1,5 @@
-import math as m
-from pathlib import Path
-
-from pandas.core.indexes.base import pprint_thing
 from utility import *
-from time import process_time_ns, time_ns
+from time import  time_ns
 
 SAMPLE = """32T3K 765
 T55J5 684
@@ -13,63 +9,56 @@ QQQJA 483"""
 
 DATA = DAY(7)
 
-Hand = namedtuple('Hand', 'setrank cardrank cards bid')
+Hand = namedtuple("Hand", "setrank cardrank cards bid")
 
 def cardset_to_setrank(cs):
-    ranks = [
-        [1, 1, 1, 1, 1],
-        [2, 1, 1, 1],
-        [2, 2, 1],
-        [3, 1, 1],
-        [3, 2],
-        [4, 1],
-        [5]
-    ]
+    ranks = [[1, 1, 1, 1, 1], [2, 1, 1, 1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
     return ranks.index(cs)
 
 def cardhand(cardstr, j_is_wild=False):
     cards, bid = cardstr.split(" ")
     bid = int(bid)
-    nj, cards_noj = sum(1 for c in cards if c == 'J'), ''.join(c for c in cards if c != 'J')
+    nj, cards_noj = sum(1 for c in cards if c == "J"), "".join(
+        c for c in cards if c != "J"
+    )
     cards_to_count = cards if not j_is_wild else cards_noj
-    cardset = sorted(Counter(cards_to_count).values(), reverse=True) if cards_to_count else [0]
+    cardset = (
+        sorted(Counter(cards_to_count).values(), reverse=True)
+        if cards_to_count
+        else [0]
+    )
     if j_is_wild:
         cardset[0] += nj
     rank = "123456789TJQKA" if not j_is_wild else "J123456789TQKA"
     cardranks = mapt(lambda x: rank.index(x), cards)
     return Hand(cardset_to_setrank(cardset), cardranks, cards, bid)
 
-def p(data, j_is_wild):
-    return sorted([cardhand(line, j_is_wild) for line in data.splitlines()])
+def parse_cards(line):
+    return cardhand(line, False)
 
-# --- part 1
-def one(data):
+def parse_cards_wild_j(line):
+    return cardhand(line, True)
+
+def score(data, parser):
     s = 0
-    for i, hand in enumerate(p(data, False)):
+    for i, hand in enumerate(parse(data, parser, lines, show=0)):
         score = hand.bid * (i + 1)
         # print(f"{hand.cards} \t\t {hand.bid} * {i + 1} = {score}")
         s += score
     return s
 
+
 start = time_ns()
-print(f"{one(SAMPLE) = :}")
-print(f"{one(DATA) = :}")
+print(f"{score(SAMPLE, parse_cards) = :}")
+print(f"{score(DATA, parse_cards) = :}")
+
 mid = time_ns()
 print(f"\t{(mid - start) / 1e6:.0f}ms")
 
 print("-----------")
 
-# --- part 2
-def two(data):
-    s = 0
-    for i, hand in enumerate(p(data, True)):
-        score = hand.bid * (i + 1)
-        # print(f"{hand.cards} \t\t {hand.bid} * {i + 1} = {score}")
-        s += score
-    return s
-
-print(f"{two(SAMPLE) = :}")
-print(f"{two(DATA) = :}")
+print(f"{score(SAMPLE, parse_cards_wild_j) = :}")
+print(f"{score(DATA, parse_cards_wild_j) = :}")
 end = time_ns()
 print(f"\t{(end - mid) / 1e6:.0f}ms")
 print(f"Total {(end - start) / 1e6:.0f} ms")
--- 2023/utility.py
+++ 2023/utility.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from itertools import *
 from itertools import chain
 from pathlib import Path
-from typing import List, Tuple, Union, Sequence, Iterable, Optional, Set
+from typing import List, Tuple, Union, Sequence, Iterable, Optional, Set, Callable
 import functools
 import math as m
 import os
@@ -18,8 +18,47 @@ import networkx as nx
 from datetime import time
 
 
-
 DAY = lambda day: Path(f"input/day{day:02d}").read_text()
+
+
+
+# PARSER
+current_year = 2022    # Subdirectory name for input files
+lines = str.splitlines # By default, split input text into lines
+def paragraphs(text): "Split text into paragraphs"; return text.split('\n\n')
+
+def parse(day_or_text:Union[int, str], parser:Callable=str, section_by:Callable=lines, show=8) -> tuple:
+    """Split the input text into `sections`, and apply `parser` to each.
+    The first argument is either the text itself, or the day number of a text file."""
+    text = get_text(day_or_text)
+    show_items('Puzzle input', text.splitlines(), show)
+    records = mapt(parser, section_by(text.rstrip()))
+    if parser != str or section_by != lines:
+        show_items('Parsed representation', records, show)
+    return records
+
+def get_text(day_or_text:Union[int, str]) -> str:
+    """The text used as input to the puzzle: either a string or the day number,
+    which denotes the file 'AOC/year/input{day}.txt'."""
+    if isinstance(day_or_text, str):
+        return day_or_text
+    else:
+        filename = f'input/day{day_or_text:02d}'
+        return Path(filename).read_text()
+
+def show_items(source, items, show:int, hr="─"*100):
+    """Show the first few items, in a pretty format."""
+    if show:
+        types = Counter(map(type, items))
+        counts = ', '.join(f'{n} {t.__name__}{"" if n == 1 else "s"}' for t, n in types.items())
+        print(f'{hr}\n{source} ➜ {counts}:\n{hr}')
+        for line in items[:show]:
+            print(truncate(line))
+        if show < len(items):
+            print('...')
+
+
+# =================================
 
 
 class ErrorTodo(Exception):
@@ -41,15 +80,6 @@ def truncate(object, width=100, ellipsis=" ...") -> str:
     return (
         string if len(string) <= width else string[: width - len(ellipsis)] + ellipsis
     )
-
-
-# === Splitting regions of text ===
-
-lines = str.splitlines
-
-
-def paragraphs(text):
-    return text.split("\n\n")
 
 
 # === Parsing parts of text ===
@@ -176,11 +206,6 @@ def clock_mod(i, m) -> int:
     return (i % m) or m
 
 
-cat = "".join
-cache = functools.lru_cache(None)
-Ø = frozenset()  # empty set
-
-
 def countwhere(pred, iterable) -> int:
     return sum(1 for item in iterable if pred(item))
 
@@ -234,6 +259,9 @@ def first_true(iterable, default=False):
     return next((x for x in iterable if x), default)
 
 
+# =========================
+# POINTS IN SPACE
+# =========================
 Point = Tuple[int, ...]  # Type for points
 Vector = Point  # E.g., (1, 0) can be a point, or can be a direction, a Vector
 Zero = (0, 0)
@@ -333,6 +361,10 @@ def taxi_distance(p: Point, q: Point) -> int:
     return abs(p[0] - q[0]) + abs(p[1] - q[1])
 
 
+# ==============
+# GRID
+# ==============
+
 class Grid(dict):
     """A 2D grid, implemented as a mapping of {(x, y): cell_contents}."""
 
@@ -403,3 +435,245 @@ def neighbors(point, directions=directions4) -> List[Point]:
     """Neighbors of this point, in the given directions.
     (This function can be used outside of a Grid class.)"""
     return [add(point, Δ) for Δ in directions]
+
+
+
+# =======
+# UTILITY
+# =======
+class multimap(defaultdict):
+    """A mapping of {key: [val1, val2, ...]}."""
+
+    def __init__(self, pairs: Iterable[tuple] = (), symmetric=False):
+        """Given (key, val) pairs, return {key: [val, ...], ...}.
+        If `symmetric` is True, treat (key, val) as (key, val) plus (val, key)."""
+        self.default_factory = list
+        for key, val in pairs:
+            self[key].append(val)
+            if symmetric:
+                self[val].append(key)
+
+
+def prod(numbers) -> float:  # Will be math.prod in Python 3.8
+    """The product formed by multiplying `numbers` together."""
+    result = 1
+    for x in numbers:
+        result *= x
+    return result
+
+
+def T(matrix: Sequence[Sequence]) -> List[Tuple]:
+    """The transpose of a matrix: T([(1,2,3), (4,5,6)]) == [(1,4), (2,5), (3,6)]"""
+    return list(zip(*matrix))
+
+
+def total(counter: Counter) -> int:
+    """The sum of all the counts in a Counter."""
+    return sum(counter.values())
+
+
+def minmax(numbers) -> Tuple[int, int]:
+    """A tuple of the (minimum, maximum) of numbers."""
+    numbers = list(numbers)
+    return min(numbers), max(numbers)
+
+
+def cover(*integers) -> range:
+    """A `range` that covers all the given integers, and any in between them.
+    cover(lo, hi) is an inclusive (or closed) range, equal to range(lo, hi + 1).
+    The same range results from cover(hi, lo) or cover([hi, lo])."""
+    if len(integers) == 1:
+        integers = the(integers)
+    return range(min(integers), max(integers) + 1)
+
+
+def the(sequence) -> object:
+    """Return the one item in a sequence. Raise error if not exactly one."""
+    for i, item in enumerate(sequence, 1):
+        if i > 1:
+            raise ValueError(f"Expected exactly one item in the sequence.")
+    return item
+
+
+def split_at(sequence, i) -> Tuple[Sequence, Sequence]:
+    """The sequence split into two pieces: (before position i, and i-and-after)."""
+    return sequence[:i], sequence[i:]
+
+
+def ignore(*args) -> None:
+    "Just return None."
+    return None
+
+
+def is_int(x) -> bool:
+    "Is x an int?"
+    return isinstance(x, int)
+
+
+def sign(x) -> int:
+    "0, +1, or -1"
+    return 0 if x == 0 else +1 if x > 0 else -1
+
+
+def lcm(i, j) -> int:
+    "Least common multiple"
+    return i * j // gcd(i, j)
+
+
+def union(sets) -> set:
+    "Union of several sets"
+    return set().union(*sets)
+
+
+def intersection(sets):
+    "Intersection of several sets; error if no sets."
+    first, *rest = sets
+    return set(first).intersection(*rest)
+
+
+def naked_plot(points, marker="o", size=(10, 10), invert=True, square=False, **kwds):
+    """Plot `points` without any axis lines or tick marks.
+    Optionally specify size, whether square or not, and whether to invery y axis."""
+    if size:
+        plt.figure(figsize=((size, size) if is_int(size) else size))
+    plt.plot(*T(points), marker, **kwds)
+    if square:
+        plt.axis("square")
+    plt.axis("off")
+    if invert:
+        plt.gca().invert_yaxis()
+
+
+def clock_mod(i, m) -> int:
+    """i % m, but replace a result of 0 with m"""
+    # This is like a clock, where 24 mod 12 is 12, not 0.
+    return (i % m) or m
+
+
+def invert_dict(dic) -> dict:
+    """Invert a dict, e.g. {1: 'a', 2: 'b'} -> {'a': 1, 'b': 2}."""
+    return {dic[x]: x for x in dic}
+
+
+def walrus(name, value):
+    """If you're not in 3.8, and you can't do `x := val`,
+    then you can use `walrus('x', val)`."""
+    globals()[name] = value
+    return value
+
+
+cat = "".join
+cache = functools.lru_cache(None)
+Ø = frozenset()  # empty set
+
+
+def quantify(iterable, pred=bool) -> int:
+    """Count the number of items in iterable for which pred is true."""
+    return sum(1 for item in iterable if pred(item))
+
+
+def dotproduct(vec1, vec2):
+    """The dot product of two vectors."""
+    return sum(map(operator.mul, vec1, vec2))
+
+
+def powerset(iterable) -> Iterable[tuple]:
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return flatten(combinations(s, r) for r in range(len(s) + 1))
+
+
+flatten = chain.from_iterable  # Yield items from each sequence in turn
+
+
+def append(sequences) -> Sequence:
+    "Append into a list"
+    return list(flatten(sequences))
+
+
+def batched(iterable, n) -> Iterable[tuple]:
+    "Batch data into non-overlapping tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    it = iter(iterable)
+    while True:
+        batch = tuple(islice(it, n))
+        if batch:
+            yield batch
+        else:
+            return
+
+
+def sliding_window(sequence, n) -> Iterable[Sequence]:
+    """All length-n subsequences of sequence."""
+    return (sequence[i : i + n] for i in range(len(sequence) + 1 - n))
+
+
+def first(iterable, default=None) -> Optional[object]:
+    """The first element in an iterable, or the default if iterable is empty."""
+    return next(iter(iterable), default)
+
+
+def last(iterable) -> Optional[object]:
+    """The last element in an iterable."""
+    for item in iterable:
+        pass
+    return item
+
+
+def nth(iterable, n, default=None):
+    "Returns the nth item or a default value"
+    return next(islice(iterable, n, None), default)
+
+
+def first_true(iterable, default=False):
+    """Returns the first true value in the iterable.
+    If no true value is found, returns `default`."""
+    return next((x for x in iterable if x), default)
+
+
+# ===============
+# Data structures
+# ===============
+class PriorityQueue:
+    """A queue in which the item with minimum key(item) is always popped first."""
+
+    def __init__(self, items=(), key=lambda x: x):
+        self.key = key
+        self.items = [] # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+
+    def add(self, item):
+        """Add item to the queue."""
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        """Pop and return the item with min f(item) value."""
+        return heapq.heappop(self.items)[1]
+
+    def top(self): return self.items[0][1]
+
+    def __len__(self): return len(self.items)
+
+class Hdict(dict):
+    """A dict, but it is hashable."""
+    def __hash__(self): return hash(tuple(sorted(self.items())))
+
+class HCounter(Counter):
+    """A Counter, but it is hashable."""
+    def __hash__(self): return hash(tuple(sorted(self.items())))
+
+class Graph(dict):
+    """A graph of {node: [neighboring_nodes...]}.
+    Can store other kwd attributes on it (which you can't do with a dict)."""
+    def __init__(self, contents, **kwds):
+        self.update(contents)
+        self.__dict__.update(**kwds)
+
+class AttrCounter(Counter):
+    """A Counter, but `ctr['name']` and `ctr.name` are the same."""
+    def __getattr__(self, attr):
+        return self[attr]
+    def __setattr__(self, attr, value):
+        self[attr] = value
