a828e8c0ede87d0957eb6ebaafa057e2d5b5442e
Chris Davison <c.jr.davison@gmail.com>
2023-12-08 07:34:10 +0000

    Start day08

---
+++ 2023/day08.py
@@ -0,0 +1,18 @@
+from utility import *
+
+SAMPLE = """"""
+
+DATA = Path("input/8").read_text()
+
+
+@timed
+def part1(data=SAMPLE):
+    pass
+
+@timed
+def part2(data=SAMPLE):
+    pass
+
+
+part1()
+part2()
