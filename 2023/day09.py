8fd5605710a13c0c9e410dfa03f5119e73d5dd41
Chris Davison <c.jr.davison@gmail.com>
2023-12-10 19:56:50 +0000

    2d class and watcher script

--- 2023/day09.py
+++ 2023/day09.py
@@ -1,11 +1,11 @@
 from utility import parse, ints, lines, timed, Path
 
+
 SAMPLE = """0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45"""
 
 DATA = Path("input/09").read_text()
-
 
 @timed
 def do(data=SAMPLE, rev_line=False):
