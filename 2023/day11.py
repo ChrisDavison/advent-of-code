1c21fedc369b61e404dcbd81dcd6ebf0f90ff867
Chris Davison <c.jr.davison@gmail.com>
2023-12-11 09:00:48 +0000

    Start day11

---
+++ 2023/day11.py
@@ -0,0 +1,24 @@
+from utility import *
+
+SAMPLE = """ """
+
+DATA = Path("input/11").read_text()
+
+
+@timed
+def part1(data=SAMPLE):
+    data = parser(data)
+    pass
+
+
+@timed
+def part2(data=SAMPLE):
+    data = parser(data)
+    pass
+
+
+def parser(data):
+    return parse(data, str, lines, show=8)
+
+part1()
+part2()
