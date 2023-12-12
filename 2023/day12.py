79f7f33863b721a798545d14207d0916d01984c1
Chris Davison <c.jr.davison@gmail.com>
2023-12-12 11:19:58 +0000

    Day12 part1, but slow

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,4 +1,5 @@
-from utility import *
+from utility import lines, parse, timed, Path, ints, re, mapt, permutations, cat
+from itertools import product
 
 SAMPLE = """???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -13,7 +14,12 @@ DATA = Path("input/12").read_text()
 @timed
 def part1(data=SAMPLE):
     data = parser(data)
-    pass
+    s = 0
+    for (pattern, to_match) in data:
+        n = get_permutations(pattern, to_match)
+        s += n
+        # print(pattern, to_match, n)
+    return s
 
 
 @timed
@@ -23,13 +29,35 @@ def part2(data=SAMPLE):
 
 
 def parser(data):
-    out = []
-    def parse_line(l):
-        s, c = l.split(' ')
+    def parse_line(line):
+        s, c = line.split(' ')
         c = ints(c)
-        return ([ch for ch in s], c)
+        return (s, c)
 
-    return parse(data, parse_line, lines)
-        
+    return parse(data, parse_line, lines, show=0)
+
+
+def count_springs(line):
+    if isinstance(line, list):
+        line = cat(line)
+    return mapt(len, re.findall('#+', line))
+
+
+def get_permutations(line, to_match):
+    q = [m.start() for m in re.finditer(r'\?', line)]
+    n = 0
+    for permut in product([True, False], repeat=len(q)):
+        l2 = list(line)
+        for i, p in enumerate(permut):
+            l2[q[i]] = '#' if p else '.'
+        if count_springs(l2) == to_match:
+            n += 1
+    return n
+
+
+
+print(count_springs(list('.##.##.###')))
+print(get_permutations('.??.??.###', (2, 2, 3)))
 part1()
+part1(12)
 # part2()
