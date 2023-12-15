016cf77feb26b07ceefbf74d0d555f3a3cc9e0f7
Chris Davison <c.jr.davison@gmail.com>
2023-12-15 09:05:07 +0000

    day15 part1

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,27 +1,24 @@
-from utility import lines, parse, Path, timed
+from utility import *
 
-SAMPLE = """ """
+SAMPLE = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
 
-# DATA = Path("input/15").read_text()
+DATA = Path("input/15").read_text()
 
+def puzzlehash(s):
+    h = 0
+    for ch in s:
+        o = ord(ch)
+        h += o
+        h *= 17
+        h %= 256
+    return h
 
-@timed
-def part1(data=SAMPLE):
-    data = parser(data)
-    pass
+timer()
+print(f"{puzzlehash('rn=1') = :}")
+print(f"{puzzlehash('HASH') = :}")
 
+P = mapt(puzzlehash, DATA.replace('\n', '').split(','))
+timer(f"Part 1: {sum(P)}")
+pyperclip.copy(sum(P))
+print(f"{sum(P) = :}")
 
-@timed
-def part2(data=SAMPLE):
-    data = parser(data)
-    pass
-
-
-def parser(data):
-    return parse(data, str, lines, show=8)
-
-
-part1()
-# part1(15)
-# part2()
-# part2(15)
