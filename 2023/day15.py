362ad0cc1389d64a1d726ef818f5928517154485
Chris Davison <c.jr.davison@gmail.com>
2023-12-15 09:31:43 +0000

    day15 part2

--- 2023/day15.py
+++ 2023/day15.py
@@ -14,11 +14,52 @@ def puzzlehash(s):
     return h
 
 timer()
-print(f"{puzzlehash('rn=1') = :}")
-print(f"{puzzlehash('HASH') = :}")
 
+# print(f"{puzzlehash('rn=1') = :}")
+# print(f"{puzzlehash('HASH') = :}")
+
+# Part 1
 P = mapt(puzzlehash, DATA.replace('\n', '').split(','))
 timer(f"Part 1: {sum(P)}")
 pyperclip.copy(sum(P))
-print(f"{sum(P) = :}")
+print()
+
+
+# Part 2
+timer(reset=True)
+P = [(part, puzzlehash(part)) for part in DATA.replace('\n', '').split(',')]
+boxes = []
+for _ in range(256):
+    boxes.append(dict())
+# print(f"{boxes = :}")
+for part, h in P:
+    m = re.search(r'(\w+)([=-])(\d+)*', part)
+    label, *action = m.groups()
+    # print(part, label, action, h)
+    match action:
+        case ('=', n):
+            box = puzzlehash(label)
+            # print(f"Update box {box} to include label {label} with lens {n}")
+            boxes[box][label] = n
+        case ('-', _):
+            # print(f"Remove label {label} from box {h}")
+            box = puzzlehash(label)
+            if label in boxes[box]:
+                del boxes[box][label]
+
+s = 0
+for i, box in enumerate(boxes, start=1):
+    # print(box)
+    if len(box) == 0:
+        continue
+    # print(i-1)
+    for j, (k, v) in enumerate(box.items(), start=1):
+        sb = i * j * int(v)
+        # print(f' {j} - {k}: {v} {sb}')
+        s += sb
+
+
+timer(f"Part 2: {s}")
+pyperclip.copy(s)
+print()
 
