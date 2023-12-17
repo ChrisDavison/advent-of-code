80dd3532c01c36cfc82999eba42f8686e37c6f08
Chris Davison <c.jr.davison@gmail.com>
2023-12-17 07:40:53 +0000

    Start day17

---
+++ 2023/day17.py
@@ -0,0 +1,28 @@
+from utility import *
+
+SAMPLE = """ """
+DATA = Path("input/17").read_text()
+
+P = []
+for line in SAMPLE.splitlines():
+    P.append(line)
+
+# part1
+timer()
+
+# ... do stuff ...
+
+res = 0
+timer(f"Part 1: {res}")
+pyperclip.copy(int(res))
+
+# part2
+timer(reset=True)
+
+# ... do stuff ...
+
+res = 0
+timer(f"Part 2: {res}")
+pyperclip.copy(int(res))
+
+
