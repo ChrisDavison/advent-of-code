fe2aee56c8c20ce0b52e4d50dfcc5273fd155ef5
Chris Davison <c.jr.davison@gmail.com>
2023-12-17 20:15:02 +0000

    day17

--- 2023/utility.py
+++ 2023/utility.py
@@ -746,3 +746,83 @@ def un_grid(grid):
 def get_dim(data):
     g = as_grid(data)
     return (len(g[0]), len(g))
+
+
+def A_star_search(problem, h=None):
+    """Search nodes with minimum f(n) = path_cost(n) + h(n) value first."""
+    h = h or problem.h
+    return best_first_search(problem, f=lambda n: n.path_cost + h(n))
+
+def best_first_search(problem, f) -> 'Node':
+    "Search nodes with minimum f(node) value first."
+    node = Node(problem.initial)
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost < reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return search_failure
+
+
+class SearchProblem:
+    """The abstract class for a search problem. A new domain subclasses this,
+    overriding `actions` and perhaps other methods.
+    The default heuristic is 0 and the default action cost is 1 for all states.
+    When you create an instance of a subclass, specify `initial`, and `goal` states 
+    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
+
+    def __init__(self, initial=None, goal=None, **kwds): 
+        self.__dict__.update(initial=initial, goal=goal, **kwds) 
+        
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(type(self).__name__, self.initial, self.goal)
+    
+    def actions(self, state):        raise NotImplementedError
+    def result(self, state, action): return action # Simplest case: action is result state
+    def is_goal(self, state):        return state == self.goal
+    def action_cost(self, s, a, s1): return 1
+    def h(self, node):               return 0 # Never overestimate!
+    
+class GridProblem(SearchProblem):
+    """Problem for searching a grid from a start to a goal location.
+    A state is just an (x, y) location in the grid."""
+    def actions(self, loc):           return self.grid.neighbors(loc)
+    def result(self, loc1, loc2):     return loc2
+    def h(self, node):                return taxi_distance(node.state, self.goal) 
+
+class Node:
+    "A Node in a search tree."
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+
+    def __repr__(self):      return f'Node({self.state}, path_cost={self.path_cost})'
+    def __len__(self):       return 0 if self.parent is None else (1 + len(self.parent))
+    def __lt__(self, other): return self.path_cost < other.path_cost
+    
+search_failure = Node('failure', path_cost=inf) # Indicates an algorithm couldn't find a solution.
+    
+def expand(problem, node):
+    "Expand a node, generating the children nodes."
+    s = node.state
+    for action in problem.actions(s):
+        s2 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s2)
+        yield Node(s2, node, action, cost)
+        
+def path_actions(node):
+    "The sequence of actions to get to this node."
+    if node.parent is None:
+        return []  
+    return path_actions(node.parent) + [node.action]
+
+def path_states(node):
+    "The sequence of states to get to this node."
+    if node in (search_failure, None): 
+        return []
+    return path_states(node.parent) + [node.state]
