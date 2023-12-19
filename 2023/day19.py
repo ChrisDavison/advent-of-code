b28decff3aa50e683918d1c5f1cf81bd4906a1a2
Chris Davison <c.jr.davison@gmail.com>
2023-12-19 07:30:19 +0000

    start fleshing out today

--- 2023/day19.py
+++ 2023/day19.py
@@ -1,13 +1,50 @@
 from collections import defaultdict
+from dataclasses import dataclass
 import re
 from utility import T, minmax, split_at, quantify, powerset, batched, sliding_window, first, last, nth, first_true, as_grid
 
-SAMPLE = """ """
+SAMPLE = """px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"""
 DATA = open("input/19").readlines()
 
-P = []
-for line in SAMPLE.splitlines():
-    P.append(line)
+data = SAMPLE
+
+@dataclass
+class Part:
+    x: int
+    m: int
+    a: int
+    s: int
+
+@dataclass
+class Rule:
+    key: str
+    predicate
+    target: str
+
+
+rules, parts = data.split('\n\n')
+rdict = dict()
+
+for line in rules.splitlines():
+    key, rr = line.split('{')
+    rrr = rr[:-1].split(',')
+
 
 print(f"Part 1: {0}")
 
