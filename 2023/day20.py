5529ed1c8cf853e58389b0de5ae1a4bb106534f2
Chris Davison <c.jr.davison@gmail.com>
2023-12-20 14:59:37 +0000

    Day 20 part 1

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,6 +1,7 @@
 from collections import defaultdict, deque
 from functools import lru_cache
 import re
+import pyperclip
 
 SAMPLE = """broadcaster -> a, b, c
 %a -> b
@@ -16,92 +17,74 @@ SAMPLE2 = """broadcaster -> a
 DATA = [line.strip() for line in open("input/20").readlines()]
 
 
-def parse(data=SAMPLE):
-    rules = dict()
-    constate = defaultdict(list)
-    ffstate = defaultdict(list)
-    q = deque([('broadcaster', 0)])
-    for line in data:
-        i, o = line.split(' -> ')
-        prefix = i[0]
-        label = i[1:]
-        o = [out.strip() for out in o.split(',')]
-        if i == 'broadcaster':
-            labels = [thing.strip() for thing in o]
-            rules['broadcaster'] = ('broadcast', labels)
-            continue
-        if prefix == '&':
-            # conjunction labels should track how many inputs they need
-            if current := constate.get(label, None):
-                constate[label] = (current[0]+1, [])
-            else:
-                constate[label] = (1, [])
+class Module:
+    def __init__(self, label, prefix, targets):
+        self.label = label
+        self.prefix = prefix
+        self.targets = targets
+
+        if self.prefix == '%':
+            self.memory = -1
         else:
-            ffstate[label] = -1
-        rules[label] = (prefix, o)
-    return rules, q, constate, ffstate
+            self.memory = {}
+
+    def __repr__(self):
+        return f"{self.prefix}{self.label} -> {self.targets} {self.memory}"
+
+def parse(data=SAMPLE):
+    start = []
+    rules = dict()
+    for line in data:
+        left, right = line.split(' -> ')
+        outs = [thing.strip() for thing in right.split(',')]
+        if left == 'broadcaster':
+            start = outs
+        else:
+            prefix = left[0]
+            label = left[1:]
+            rules[label] = Module(label, prefix, outs)
+    
+    for name, module in rules.items():
+        for target in module.targets:
+            if target in rules and rules[target].prefix == '&':
+                rules[target].memory[name] = -1
+    
+    return rules, start
 
 
-def step(rules, q, constate, ffstate):
+def part1(data=SAMPLE, n=1):
+    rules, start_targets = parse(data)
     n_lo = n_hi = 0
-    n_lo += 1
-    q = q.copy()
-    while q:
-        target, val = q.popleft()
-        if target not in rules:
-            print(target, 'not in rules')
-            continue
-        prefix, out = rules[target]
-        if prefix == 'broadcast':
-            for o in out:
-                # print(f"broadcaster -low-> {o}")
-                q.append((o, -1))
-                n_lo += 1
-        elif prefix == '%':
+    for i in range(n):
+        n_lo += 1
+        q = deque([('broadcaster', target, -1) for target in start_targets])
+        q = q.copy()
+
+        while q:
+            op, target, val = q.popleft()
+
             if val == 1:
-                # print(f"\t{target} -high-> NOOP")
+                n_hi += 1
+            else:
+                n_lo += 1
+
+            if target not in rules:
+                # print(target, 'not in rules')
                 continue
-            for o in out:
-                q.append((o, -ffstate[target]))
-                ss = '-low' if -ffstate[target] == -1 else '-high'
-                # print(f"{target} {ss}-> {o}")
-            ffstate[target] = -ffstate[target]
-            if -ffstate[target] == -1:
-                n_lo += len(out)
-            else:
-                n_hi += len(out)
-        elif prefix == '&':
-            n, conq = constate[target]
-            conq.append(val)
-            # print(f"{conq = :}")
-            if len(conq) == n:
-                # if we've got a full queue for this conjunction node,
-                # process it
-                outval = -val
-                if n > 1:
-                    outval = -1 if all(x == 1 for x in conq) else 1
-                for o in out:
-                    q.append((o, outval))
-                if outval == 1:
-                    n_hi += len(out)
-                else:
-                    n_lo += len(out)
-                ss = '-low' if outval == -1 else '-high'
-                # print(f"{target} {ss}-> {out}")
-                constate[target] = (n, [])
-            else:
-                # just add the signal to the queue
-                constate[target] = (n, conq + [val])
-                # print(f"{target} acc append {val}")
-    return rules, q, constate, ffstate, n_lo, n_hi
 
+            module = rules[target]
+            # print(f"{module = :}")
 
-def part1(data=SAMPLE):
-    rules, q, constate, ffstate = parse(data)
-    n_lo = n_hi = 0
-    for i in range(1000):
-        rules, _, constate, ffstate, lo, hi = step(rules, q, constate, ffstate)
-        n_lo += lo
-        n_hi += hi
+            if module.prefix == '%':
+                if val == -1:
+                    module.memory = -module.memory
+                    for o in module.targets:
+                        q.append((module.label, o, module.memory))
+            elif module.prefix == '&':
+                module.memory[op] = val
+                outval = -1 if all(x == 1 for x in module.memory.values()) else 1
+                for o in module.targets:
+                    q.append((module.label, o, outval))
     print(f"{n_lo = :}, {n_hi = :}")
     print(f"Part 1: {n_lo * n_hi = :}")
+    pyperclip.copy(n_lo * n_hi)
