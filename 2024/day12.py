5222e618e95750c2fc49ea54e19dba03f2e8567e
Chris Davison <c.jr.davison@gmail.com>
2024-12-12 09:37:07 +0000

    parse graph

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,6 +1,24 @@
 import utility as u
+from collections import defaultdict
 from pathlib import Path
 from argparse import ArgumentParser
+import numpy as np
+
+
+def dfs(graph, start, ch, visited=None):
+    if visited is None:
+        visited = set()
+
+    if graph[start] != ch:
+        return 0
+
+    visited.add(start)
+    pathcount = 0
+    for xy in u.neighbors2(start):
+        if xy not in visited:
+            pathcount += dfs(graph, xy, ch, visited)
+    visited.remove(start)
+    return pathcount
 
 
 if __name__ == "__main__":
@@ -23,7 +41,20 @@ if __name__ == "__main__":
     else:
         data = input("input: ")
 
-    # ----------------------------------------
-    #                SOLUTION
-    # ----------------------------------------
+    # ------------------------------------------------------------
+    #                        generate graph
+    # ------------------------------------------------------------
+    graph = defaultdict(list)
+    grid = np.array([[ch for ch in row] for row in data.splitlines()])
+    for x, row in enumerate(grid):
+        for y, ch in enumerate(row):
+            for x2, y2 in u.neighbors2((x, y)):
+                if x2 >= 0 and x2 < grid.shape[0] and y2 >= 0 and y2 < grid.shape[1]:
+                    if grid[x2][y2] == ch:
+                        graph[(x, y)].append((x2, y2))
+
+    # ------------------------------------------------------------
+    #                             solve
+    # ------------------------------------------------------------
     print(data)
+    print(graph)
