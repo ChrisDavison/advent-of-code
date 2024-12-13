9c24016819a7d9d41f0e5cb60f3c8617b9bfc587
Chris Davison <c.jr.davison@gmail.com>
2024-12-13 09:51:35 +0000

    2024 day13 part1

--- 2024/day13.py
+++ 2024/day13.py
@@ -1,28 +1,50 @@
 import utility as u
 from pathlib import Path
 from argparse import ArgumentParser
+import numpy as np
+
+
+def cost(a, b, prize, na, nb):
+    lhs = na * a + nb * b
+    if not np.all(lhs == prize):
+        return None
+    return (na * 3,)
+
+
+def solve(a, b, prize, prices=np.array([3, 1]), tokens=100):
+    minprice = None
+    for na in range(tokens):
+        for nb in range(tokens):
+            lhs = na * a + nb * b
+            # print(f"{lhs}")
+            if not np.all(lhs == prize):
+                continue
+            price = na * prices[0] + nb * prices[1]
+            if minprice:
+                minprice = min(price, minprice)
+            else:
+                minprice = price
+    return minprice or 0
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    mode = parser.add_mutually_exclusive_group()
-    mode.add_argument("-s", "--sample", action="store_true")
-    mode.add_argument("-d", "--data", action="store_true")
-    parser.add_argument("-n", default=25, type=int)
-    parser.add_argument("--debug", action="store_true", default=False)
-    parser.add_argument("input", nargs="*")
+    parser.add_argument("-d", "--debug", action="store_true", default=False)
+    parser.add_argument("file", type=Path, nargs=1)
     args = parser.parse_args()
 
     DAYNUM = u.ints(Path(__file__).stem)[0]
-    if args.sample:
-        data = Path(f"input/{DAYNUM:02d}s").read_text().strip()
-    elif args.data:
-        data = Path(f"input/{DAYNUM:02d}").read_text().strip()
-    elif args.input:
-        data = " ".join(args.input)
-    else:
-        data = input("input: ")
+    data = u.paragraphs(args.file[0].read_text().strip())
 
-    # ----------------------------------------
-    #                SOLUTION
-    # ----------------------------------------
+    parsed = [
+        u.mapl(lambda x: np.array(x, dtype=int), u.mapl(u.ints, chunk.splitlines()))
+        for chunk in data
+    ]
+
+    # ---------------------------------------- Solution
+    min_tokens = 0
+    for p in parsed:
+        n_tokens = solve(*p)
+        if n_tokens:
+            min_tokens += n_tokens
+    print(min_tokens)
--- 2024/template.py
+++ 2024/template.py
@@ -5,24 +5,11 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    mode = parser.add_mutually_exclusive_group()
-    mode.add_argument("-s", "--sample", action="store_true")
-    mode.add_argument("-d", "--data", action="store_true")
-    parser.add_argument("-n", default=25, type=int)
-    parser.add_argument("--debug", action="store_true", default=False)
-    parser.add_argument("input", nargs="*")
+    parser.add_argument("-d", "--debug", action="store_true", default=False)
+    parser.add_argument("file", type=Path, nargs=1)
     args = parser.parse_args()
 
     DAYNUM = u.ints(Path(__file__).stem)[0]
-    if args.sample:
-        data = Path(f"input/{DAYNUM:02d}s").read_text().strip()
-    elif args.data:
-        data = Path(f"input/{DAYNUM:02d}").read_text().strip()
-    elif args.input:
-        data = " ".join(args.input)
-    else:
-        data = input("input: ")
+    data = args.file[0].read_text().strip()
 
-    # ----------------------------------------
-    #                SOLUTION
-    # ----------------------------------------
+    # ---------------------------------------- Solution
