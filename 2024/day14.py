8f33eabc4e80a61af2c01a2808e78679a0817b87
Chris Davison <c.jr.davison@gmail.com>
2024-12-14 16:06:47 +0000

    tidy up

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,30 +1,9 @@
 import utility as u
-import tqdm
-import os
-import matplotlib.pyplot as plt
-from scipy.stats import entropy
-import time
 import numpy as np
 from pathlib import Path
 from collections import defaultdict
 from argparse import ArgumentParser
 from dataclasses import dataclass
-
-
-def quadrant(position, limits):
-    midy, midx = np.floor(limits / 2).astype(int).tolist()
-    x, y = position
-    if x == midx or y == midy:
-        return -1
-    if x < midx:
-        if y < midy:
-            return 0
-        return 1
-    elif x > midx:
-        if y < midy:
-            return 2
-        return 3
-    return -1
 
 
 @dataclass(slots=True, kw_only=True)
@@ -35,6 +14,22 @@ class Robot:
     def move(self, limits, steps=1):
         delta = self.delta * steps
         self.xy = (self.xy + delta) % limits
+
+    def quadrant(self, limits):
+        midy, midx = np.floor(limits / 2).astype(int).tolist()
+        x, y = self.xy
+        if x == midx or y == midy:
+            return -1
+        return (2 if x > midx else 0) + (1 if y > midy else 0)
+
+
+def display(grid):
+    print(
+        "\n".join(
+            "".join(str(int(thing)) if thing >= 0 else "." for thing in row)
+            for row in grid
+        )
+    )
 
 
 if __name__ == "__main__":
@@ -53,14 +48,7 @@ if __name__ == "__main__":
 
     # ---------------------------------------- Solution
 
-    def display(grid):
-        print(
-            "\n".join(
-                "".join(str(int(thing)) if thing >= 0 else "." for thing in row)
-                for row in grid
-            )
-        )
-
+    # part1
     steps = 100
     limits = np.array([103, 101])
     positions = defaultdict(int)
@@ -71,34 +59,17 @@ if __name__ == "__main__":
         r.move(limits, steps)
         positions[tuple(r.xy)] += 1
 
-    for col in range(limits[1]):
-        for row in range(limits[0]):
-            g[row, col] = quadrant((row, col), limits[::-1])
-
     for r in sorted(robots, key=lambda x: tuple(x.xy)):
-        quad = quadrant(r.xy, limits[::-1])
+        quad = r.quadrant(limits[::-1])
         if quad >= 0:
             quadrants[quad] += 1
 
-    print(f"{np.prod(quadrants).astype(int)}")
+    print(f"part1 {np.prod(quadrants).astype(int)}")
 
+    # part2
     print()
-    print(f"{len(robots)=}")
-    limits = np.array([103, 101])
-    prev_xdist = None
-    prev_ydist = None
-    stds = np.zeros(10)
-    prev_entropy = 0
-    entropies = []
     entx = []
     enty = []
-
-    robots = []
-    for line in args.file[0].read_text().strip().splitlines():
-        nums = u.ints(line)
-        robots.append(
-            Robot(xy=np.array([nums[1], nums[0]]), delta=np.array([nums[3], nums[2]]))
-        )
     for i in range(1, 10000):
         g = np.zeros(limits) - 1
         xs, ys = [], []
@@ -109,13 +80,13 @@ if __name__ == "__main__":
             ys.append(r.xy[1])
         ex = int(np.var(xs))
         ey = int(np.var(ys))
+        entx.append(ex)
+        enty.append(ey)
 
         threshold = 0.8
 
         if ex < (threshold * np.mean(entx)) and ey < (threshold * np.mean(enty)):
-            print(i, ex, ey)
+            # add 'steps' to the i-count so we don't need to reset the robots
+            print(i + steps, ex, ey)
             display(g)
-            # break
             print()
-        entx.append(ex)
-        enty.append(ey)
