f99a6021e7e0960fb3fb64c4e484d9cf3e5539ac
Chris Davison <c.jr.davison@gmail.com>
2024-12-14 13:41:48 +0000

    day14 part2

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,6 +1,10 @@
 import utility as u
+import tqdm
+import os
+import matplotlib.pyplot as plt
+from scipy.stats import entropy
+import time
 import numpy as np
-import sys
 from pathlib import Path
 from collections import defaultdict
 from argparse import ArgumentParser
@@ -77,3 +81,41 @@ if __name__ == "__main__":
             quadrants[quad] += 1
 
     print(f"{np.prod(quadrants).astype(int)}")
+
+    print()
+    print(f"{len(robots)=}")
+    limits = np.array([103, 101])
+    prev_xdist = None
+    prev_ydist = None
+    stds = np.zeros(10)
+    prev_entropy = 0
+    entropies = []
+    entx = []
+    enty = []
+
+    robots = []
+    for line in args.file[0].read_text().strip().splitlines():
+        nums = u.ints(line)
+        robots.append(
+            Robot(xy=np.array([nums[1], nums[0]]), delta=np.array([nums[3], nums[2]]))
+        )
+    for i in range(1, 10000):
+        g = np.zeros(limits) - 1
+        xs, ys = [], []
+        for r in robots:
+            r.move(limits, 1)
+            g[r.xy[0], r.xy[1]] = "1"
+            xs.append(r.xy[0])
+            ys.append(r.xy[1])
+        ex = int(np.var(xs))
+        ey = int(np.var(ys))
+
+        threshold = 0.8
+
+        if ex < (threshold * np.mean(entx)) and ey < (threshold * np.mean(enty)):
+            print(i, ex, ey)
+            display(g)
+            # break
+            print()
+        entx.append(ex)
+        enty.append(ey)
