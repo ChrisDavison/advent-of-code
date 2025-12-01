c87f04e551f4cb0988f197a6a319d81a401149c2
Chris Davison <c.jr.davison@gmail.com>
2025-12-01 13:44:41 +0000

    wip 2025.1.2

--- 2025/day01.py
+++ 2025/day01.py
@@ -2,19 +2,53 @@ from utility import *
 import numpy as np
 import math
 
-data = parse("01s")
-# #data = parse("01")
+data = parse("01s", show=False)
+data = parse("01", show=False)
 
 vals = [(row[0], int(row[1:])) for row in data]
-vals = [(r, v if r == 'R' else -v) for r, v in vals]
 
 # part1
 start = 50
 part1 = 0
 for dir, val in vals:
-    start += val
+    if dir == 'R':
+        start += val
+    else:
+        start -= val
     start %= 100
     if start == 0:
         part1 += 1
 print(f"{part1 = }")
-print("░" * 80)
+print()
+
+def inc(start, value, direction):
+    if direction == 'R':
+        final = (start + value) % 100
+        does_wrap = value > (100 - start) or start == 0
+        rem = value - (100 - start)
+    else:
+        final = (start - value) % 100
+        does_wrap = value > start and start != 0
+        rem = value - start
+    zeroes = 1 if final == 0 else 0
+    arrow = {'L': "←", "R": "→"}[direction]
+
+    if does_wrap:
+        zeroes += 1+int(rem / 100)
+        print(f"{direction}{value}\t{start:>2d} {arrow} {final:<2d} \t{zeroes} ({rem})", end=' \t')
+    else:
+        print(f"{direction}{value}\t{start:>2d} {arrow} {final:<2d} \t{zeroes}", end=' \t')
+
+    return final, zeroes
+
+# part2
+print("dir, S, E, 0, rem")
+start = 50
+part2 = 0
+for dir, val in vals:
+    sym = '+' if dir == 'R' else '-'
+    final, wraps = inc(start, val, dir)
+    start = final
+    part2 += wraps
+    print(part2)
+print(f"{part2 = }")
