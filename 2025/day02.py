4488c01f8e2f3f78891b518bb457ddbf48991b68
Chris Davison <c.jr.davison@gmail.com>
2025-12-02 13:15:09 +0000

    refine 2025.2

--- 2025/day02.py
+++ 2025/day02.py
@@ -1,29 +1,31 @@
 from utility import *
 
-data = parse("02s", positive_ints, lambda x: x.split(','), show=False)
+# data = parse("02s", positive_ints, lambda x: x.split(','), show=False)
 data = parse("02", positive_ints, lambda x: x.split(','), show=False)
 
-@cache
 def is_invalid(num):
     ns = str(num)
     na = ns[:len(ns)//2]
     nb = ns[len(ns)//2:]
     return na == nb
 
-@cache
 def is_invalid_b(num):
     rx = re.compile(r'^(\d+)\1+$')
     return rx.match(str(num)) is not None
 
 
-inv = list()
-inv2 = list()
-for a, b in data:
-    for val in range(a, b+1):
-        if is_invalid(val):
-            inv.append(val)
-        if is_invalid_b(val):
-            inv2.append(val)
-print("part1:", sum(inv))
-print("part2:", sum(inv2))
+def run():
+    inv = inv2 = 0
+    for a, b in data:
+        for val in range(a, b+1):
+            if is_invalid(val):
+                inv += val
+            if is_invalid_b(val):
+                inv2 += val
+    print("part1:", inv)
+    print("part2:", inv2)
 
+if __name__ == "__main__":
+    run()
+
+# is_invalid_c(9998888999888)
