8e48a49052220269386b5b77c596c7db19cfd623
Chris Davison <c.jr.davison@gmail.com>
2025-12-02 09:25:11 +0000

    2025.2.2

--- 2025/day02.py
+++ 2025/day02.py
@@ -10,11 +10,20 @@ def is_invalid(num):
     nb = ns[len(ns)//2:]
     return na == nb
 
+@cache
+def is_invalid_b(num):
+    rx = re.compile(r'^(\d+)\1+$')
+    return rx.match(str(num)) is not None
 
 
 inv = list()
+inv2 = list()
 for a, b in data:
     for val in range(a, b+1):
         if is_invalid(val):
             inv.append(val)
+        if is_invalid_b(val):
+            inv2.append(val)
 print("part1:", sum(inv))
+print("part2:", sum(inv2))
+
