ce29a6423bd52ee9ff121f7872bd33d9a32ec161
Chris Davison <c.jr.davison@gmail.com>
2025-12-04 13:47:08 +0000

    2025.4.2 slow

--- 2025/day04.py
+++ 2025/day04.py
@@ -16,7 +16,29 @@ def part1(grid):
     print(len(valid))
 
 
+def part2(grid):
+    removals = 0
+    changed = True
+    while changed:
+        changed = False
+        for point in grid:
+            if grid[point] == '.':
+                continue
+            n_nearby = sum(p != '.' for p in grid.neighbor_contents(point))
+            # print(point, n_nearby)
+            if n_nearby < 4:
+                removals += 1
+                grid[point] = '.'
+                changed = True
+                # grid.print(highlight=[point])
+                # print()
+                break
+    print(removals)
+
+
 part1(ds)
 part1(dd)
 # print()
 # data.print()
+part2(ds)
+part2(dd)
