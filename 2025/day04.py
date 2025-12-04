bb91b4632fde76a585b779e3f9c137582d49fa55
Chris Davison <c.jr.davison@gmail.com>
2025-12-04 07:47:14 +0000

    refactor into function before part2

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,22 +1,22 @@
 from utility import *
 
-data = Grid({(j, i):  col  for i, row in enumerate(parse("04s", show=False)) for j, col in enumerate(row)}, directions=directions8)
-data = Grid({(i, j):  col  for i, row in enumerate(parse("04", show=False)) for j, col in enumerate(row)}, directions=directions8)
-#data = parse("04")
+ds = Grid({(j, i):  col  for i, row in enumerate(parse("04s", show=False)) for j, col in enumerate(row)}, directions=directions8)
+dd = Grid({(i, j):  col  for i, row in enumerate(parse("04", show=False)) for j, col in enumerate(row)}, directions=directions8)
 
-# data.print()
+def part1(grid):
+    valid = set()
+    for point in grid:
+        n_nearby = sum(p != '.' for p in grid.neighbor_contents(point))
+        # print(point, n_nearby)
+        if grid[point] == '.':
+            continue
+        if n_nearby < 4:
+            valid.add(point)
+            grid[point] = 'x'
+    print(len(valid))
 
-valid = set()
-for point in data:
-    n_nearby = sum(p != '.' for p in data.neighbor_contents(point))
-    # print(point, n_nearby)
-    if data[point] == '.':
-        continue
-    if n_nearby < 4:
-        valid.add(point)
-        data[point] = 'x'
-print(len(valid))
 
+part1(ds)
+part1(dd)
 # print()
 # data.print()
-#
