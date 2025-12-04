228e0b0acb94e80167483a3debbb6218090d5e4b
Chris Davison <c.jr.davison@gmail.com>
2025-12-04 18:44:24 +0000

    tidy up

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,13 +1,13 @@
 from utility import *
 
-ds = Grid({(j, i):  col  for i, row in enumerate(parse("04s", show=False)) for j, col in enumerate(row)}, directions=directions8)
-dd = Grid({(i, j):  col  for i, row in enumerate(parse("04", show=False)) for j, col in enumerate(row)}, directions=directions8)
+def parsegrid(s):
+    return Grid({(j, i):  col  for i, row in enumerate(parse(s, show=False)) for j, col in enumerate(row)}, directions=directions8)
+
 
 def part1(grid):
     valid = set()
     for point in grid:
         n_nearby = sum(p != '.' for p in grid.neighbor_contents(point))
-        # print(point, n_nearby)
         if grid[point] == '.':
             continue
         if n_nearby < 4:
@@ -21,24 +21,16 @@ def part2(grid):
     changed = True
     while changed:
         changed = False
-        for point in grid:
-            if grid[point] == '.':
-                continue
+        for point in grid.findall('@'):
             n_nearby = sum(p != '.' for p in grid.neighbor_contents(point))
-            # print(point, n_nearby)
             if n_nearby < 4:
                 removals += 1
                 grid[point] = '.'
                 changed = True
-                grid.print(highlight=[point])
-                print()
-                # break
     print(removals)
 
 
-part1(ds)
-part1(dd)
-# print()
-# data.print()
-part2(ds)
-part2(dd)
+part1(parsegrid("04s"))
+part1(parsegrid("04"))
+part2(parsegrid("04s"))
+part2(parsegrid("04"))
