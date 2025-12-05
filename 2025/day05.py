113d741f965abc7caa0d5a2841ee88057cae3c37
Chris Davison <c.jr.davison@gmail.com>
2025-12-05 11:49:15 +0000

    2025.5.2

--- 2025/day05.py
+++ 2025/day05.py
@@ -1,48 +1,65 @@
 from utility import *
-from collections import deque
 
 def parser(inp):
     ds = [[positive_ints(line) for line in chunk.splitlines()] for chunk in open(inp).read().split("\n\n")]
-    ranges = ds[0]
+    ranges = [list(d) for d in ds[0]]
     vals = [x[0] for x in ds[1]]
-    return ranges, vals
+    return simplify_ranges(ranges), vals
 
 
 def simplify_ranges(ranges):
-    ranges = sorted(ranges)
-    # print(ranges)
-    candidates = deque(ranges[1:])
+    def intersect(a,b):
+        if b[0]<a[0]:
+            # b0...a0
+            return intersect(b,a)
+        # a0...b0...a1 ?
+        return b[0]<=a[1]
+
+    def union(a,b):
+        if b[0]<a[0]:
+            # b0...a0
+            return union(b,a)
+        # a0....max[a1, b1]
+        return (a[0],max(a[1],b[1]))
+
     simplified = [ranges[0]]
-    cs, ce = ranges[0]
-    while candidates:
-        rs, re = candidates.popleft()
-        # print(f"{cs=} {ce=} {rs=} {re=}")
-        if rs >= cs and rs <= ce:
-            # range starts within current range
-            simplified[-1] = [cs, max(ce, re)]
-        else:
-            simplified.append([rs, re])
-            cs, ce = rs, re
-    # print(simplified)
+    for original in ranges[1:]:
+        # build a new 'simplified ranges' list
+        new_rr = []
+        for simpler in simplified:
+            if intersect(simpler,original):
+                # we can simplify by merging two ranges
+                original = union(simpler,original)
+            else:
+                # otherwise, this range _might_ be standalone
+                new_rr.append(simpler)
+        new_rr.append(original)
+        simplified = new_rr
     return simplified
+
+
 
 def part1(inp):
     ranges, vals = parser(inp)
-    final = simplify_ranges(ranges)
-    # print(f"{len(ranges)=}, {len(final)=}, {len(vals)=}")
     tot = 0
     for val in vals:
-        for rs, re in final:
-            if val >= rs and val <= re:
+        for (rs, re) in ranges:
+            if rs <= val <= re:
                 tot += 1
                 break
     print(tot)
-    # print(sum(1 for v in vals if v in final))
 
-def part2(data):
-    pass
+def part2(inp):
+    ranges, _vals = parser(inp)
+    tot = 0
+    for rs, re in ranges:
+        tot += re - rs + 1
+    print(tot)
+
 
 part1("input/05s")
 part1("input/05")
 
-simplify_ranges([(3, 10), (8, 15), (18, 20)])
+part2("input/05s")
+part2("input/05")
+
