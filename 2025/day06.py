45a7ea32841edd6588c533fe20092a7d403e25af
Chris Davison <c.jr.davison@gmail.com>
2025-12-08 23:07:11 +0000

    2025.6.2 ugly

--- 2025/day06.py
+++ 2025/day06.py
@@ -1,4 +1,6 @@
 from utility import *
+import numpy as np
+from functools import reduce
 
 def pparse(filestr):
     ds = parse(filestr, atoms, show=False)[:-1]
@@ -9,7 +11,6 @@ def part1(data, sym):
     out = [0] * len(data[0])
     for i, row in enumerate(data):
         for col, (s, val) in enumerate(zip(sym, row)):
-            # print(col, s, val)
             if s == "*":
                 if i == 0:
                     out[col] = val
@@ -19,8 +20,43 @@ def part1(data, sym):
                 out[col] += val
     print(sum(out))
 
-def part2(data):
+
+def change_endian(values):
     pass
+
+def part2(fname):
+    lines = open("input/" + fname).read().splitlines()
+
+    def yield_column(lines):
+        symbols = re.split(r"\s+", lines[-1].strip())
+        lines = lines[:-1]
+        while lines[0]:
+            newpos = 0
+            for l in lines:
+                np = l.find(' ')
+                if np > newpos:
+                    newpos = np
+                if np == -1:
+                    newpos = len(l)
+                    break
+            col = [l[:newpos] for l in lines]
+            lines = [l[newpos+1:] for l in lines]
+            sym, symbols = symbols[0], symbols[1:]
+            yield col, sym
+
+    tot = 0
+    for col, s in yield_column(lines):
+        t = [""] * len(col[0])
+        for i in range(len(t)):
+            for val in col:
+                t[i] += val[i]
+        t = list(map(int, t))
+        if s == "*":
+            val = reduce(lambda acc, x: acc * x, t[1:], t[0])
+        else:
+            val = sum(t)
+        tot += val
+    print(tot)
 
 ds, dss = pparse("06s")
 dd, dds = pparse("06")
@@ -29,3 +65,5 @@ part1(ds, dss)
 part1(dd, dds)
 print()
 
+part2("06s")
+part2("06")
