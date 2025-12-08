035141ae7e8e2aaeaa6ffb6305570d94ed2d26ef
Chris Davison <c.jr.davison@gmail.com>
2025-12-08 21:28:22 +0000

    2025.7 and 2025.8 templates

---
+++ 2025/day07.py
@@ -0,0 +1,12 @@
+from utility import *
+
+ds = parse("07s")
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+part1(ds)
+
---
+++ 2025/day08.py
@@ -0,0 +1,12 @@
+from utility import *
+
+ds = parse("08s")
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+part1(ds)
+
