a23b55439d80b5fe251c14e6aee725db1ad3e0f7
Chris Davison <c.jr.davison@gmail.com>
2025-12-10 23:16:52 +0000

    2025.9.2 using shapely

--- 2025/day09.py
+++ 2025/day09.py
@@ -1,5 +1,6 @@
 from utility import *
-from itertools import permutations
+from itertools import permutations, combinations
+from shapely import Polygon, box
 
 ds = [Point2D(p[0], p[1]) for p in parse("09s", ints, show=False)]
 dd = [Point2D(p[0], p[1]) for p in parse("09", ints, show=False)]
@@ -20,18 +21,36 @@ def part1(data):
 
 def find_squares(data):
     def is_valid(xy):
-        return xy
+        a, b = xy
+        if a.x == b.x or a.y == b.y:
+            return True
+        return True
 
+    max_x = max(p2.x - p1.x for p1, p2 in combinations(data, 2))
+    max_y = max(p2.y - p1.y for p1, p2 in combinations(data, 2))
+    print(f"{max_x, max_y = } {max_x * max_y}")
     corners = sorted(combinations(data, 2), key=lambda x: area(*x), reverse=True)
-    valid = map(is_valid, corners)
-    return area(*next(valid))
+    valid = filter(is_valid, corners)
+    n = next(valid)
+    print(f"{n = }")
+    return area(*n)
 
 def part2(data):
-    a = 0
-    print(find_squares(data))
+    def area(edge) -> int:
+        ((x1, y1), (x2, y2)) = edge
+        return (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
+
+    data = [tuple((d.x, d.y)) for d in data]
+    polygon = Polygon(data)
+
+    for edge in sorted(combinations(data, 2), key=area, reverse=True):
+        (x1, y1), (x2, y2) = edge
+        if polygon.contains(box(x1, y1, x2, y2)):
+            print(area(edge))
+            return area(edge)
 
 part1(ds)
 part1(dd)
 
 part2(ds)
-# part2(dd)
+part2(dd)
