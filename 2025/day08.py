fc63605656014b5b8955c5de23b18558cabb4c53
Chris Davison <c.jr.davison@gmail.com>
2025-12-10 00:13:45 +0000

    2025.8.1 partial

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,12 +1,57 @@
 from utility import *
+from functools import reduce
 
-ds = parse("08s")
+ds = list(enumerate(parse("08s",positive_ints)))
+dd = list(enumerate(parse("08",positive_ints, show=False)))
 
-def part1(data):
-    pass
+def part1(data, lim):
+    distances = sorted([(distance(p1, p2), i, j) for i, p1 in data for j, p2 in data if i != j])
+    boxes = []
+    for _ in range(lim):
+        # print()
+        # print(f"{boxes = }")
+        top = None
+        for _, l, r in distances:
+            if any(l in bb and r in bb for bb in boxes):
+                continue
+            else:
+                top = (l, r)
+                break
+        l_in = r_in = -1
+        print(f"{top = }")
+        assert top is not None
+        for i, b in enumerate(boxes):
+            # print(f"{b = }")
+            if top[0] in b:
+                l_in = i
+
+            if top[1] in b:
+                r_in = i
+
+        print(f"{l_in = } {r_in = }")
+        print(f"{boxes = }")
+        if l_in >=0 and r_in >= 0:
+            jb = boxes.pop(r_in)
+            boxes[l_in].extend(jb)
+        elif l_in >= 0:
+            boxes[l_in].append(top[1])
+        elif r_in >= 0:
+            boxes[r_in].append(top[0])
+        else:
+            # create new box
+            boxes.append([top[0], top[1]])
+    
+    # print("-"*40)
+    # for b in boxes:
+    #     print(f"{b = } {len(b)}")
+    #
+    lens = sorted([len(b) for b in boxes])
+    # print(f"{lens = }")
+    print(reduce(lambda acc, x: x * acc, lens[-3:], 1))
 
 def part2(data):
     pass
 
-part1(ds)
+part1(ds, lim=9)
+# part1(dd, lim=999)
 
