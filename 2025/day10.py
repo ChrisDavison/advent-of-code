de5e73ebea4a10d357caefde7d2a52f4337246c9
Chris Davison <c.jr.davison@gmail.com>
2025-12-10 09:49:06 +0000

    2025.10 template

---
+++ 2025/day10.py
@@ -0,0 +1,21 @@
+from utility import *
+
+
+def part1(data):
+    result = 0
+    return result
+
+def part2(data):
+    result = 0
+    return result
+
+ds = parse("10s")
+dd = parse("10s", show=False)
+
+header(10, 1)
+print(part1(ds))
+print(part1(dd))
+
+header(10, 2)
+print(part2(ds))
+print(part2(dd))
