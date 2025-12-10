7e897b29ab2d2881265735ab8d60a9aebd5e6159
Chris Davison <c.jr.davison@gmail.com>
2025-12-10 09:48:58 +0000

    2025.8.1

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,56 +1,38 @@
 from utility import *
 from functools import reduce
 
-ds = list(enumerate(parse("08s",positive_ints)))
+ds = list(enumerate(parse("08s",positive_ints, show=False)))
 dd = list(enumerate(parse("08",positive_ints, show=False)))
 
 def part1(data, lim):
-    distances = sorted([(distance(p1, p2), i, j) for i, p1 in data for j, p2 in data if i != j])
+    distances = sorted([(distance(p1, p2), i, j) for (i, p1), (j, p2) in combinations(data, 2)])
     boxes = []
-    for _ in range(lim):
-        # print()
-        # print(f"{boxes = }")
-        top = None
-        for _, l, r in distances:
-            if not any(l in bb and r in bb for bb in boxes):
-                top = (l, r)
-                break
+    for (_d, l, r) in distances[:lim]:
         l_in = r_in = -1
-        # print(f"{top = }")
-        assert top is not None
         for i, b in enumerate(boxes):
-            # print(f"{b = }")
-            if top[0] in b:
+            if l in b:
                 l_in = i
-
-            if top[1] in b:
+            if r in b:
                 r_in = i
-
-        # print(f"{l_in = } {r_in = }")
-        # print(f"{boxes = }")
-        # print(f"{len(boxes) = }")
-        if l_in >=0 and r_in >= 0:
+        if l_in == r_in and l_in != -1:
+            continue
+        if l_in >= 0 and r_in >= 0:
             boxes[l_in].extend(boxes[r_in])
             del boxes[r_in]
         elif l_in >= 0:
-            boxes[l_in].append(top[1])
+            boxes[l_in].append(r)
         elif r_in >= 0:
-            boxes[r_in].append(top[0])
+            boxes[r_in].append(l)
         else:
             # create new box
-            boxes.append([top[0], top[1]])
+            boxes.append([l, r])
     
-    # print("-"*40)
-    # for b in boxes:
-    #     print(f"{b = } {len(b)}")
-    #
     lens = sorted([len(b) for b in boxes])
-    # print(f"{lens = }")
     print(reduce(lambda acc, x: x * acc, lens[-3:], 1))
 
 def part2(data):
     pass
 
-part1(ds, lim=9)
-part1(dd, lim=999)
+part1(ds, lim=10)
+part1(dd, lim=1000)
 
